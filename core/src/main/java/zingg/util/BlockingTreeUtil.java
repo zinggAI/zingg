2759f0fa5a97afaf94723f5e72f18632bb03457e
Navin Singh <navin.rathore@gmail.com>
2022-03-14 10:29:30 +0530

    BlockingTreeUtil - createDataFrame from block data

--- core/src/main/java/zingg/util/BlockingTreeUtil.java
+++ core/src/main/java/zingg/util/BlockingTreeUtil.java
@@ -1,21 +1,18 @@
 package zingg.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataType;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
-import org.apache.spark.storage.StorageLevel;
+import com.snowflake.snowpark_java.DataFrame;
+import com.snowflake.snowpark_java.Row;
+import com.snowflake.snowpark_java.Session;
+import com.snowflake.snowpark_java.types.DataType;
+import com.snowflake.snowpark_java.types.DataTypes;
+import com.snowflake.snowpark_java.types.StructField;
+import com.snowflake.snowpark_java.types.StructType;
 
 import zingg.block.Block;
 import zingg.block.Canopy;
@@ -32,12 +29,12 @@ public class BlockingTreeUtil {
     public static final Log LOG = LogFactory.getLog(BlockingTreeUtil.class);
 	
 
-    public static Tree<Canopy> createBlockingTree(Dataset<Row> testData,  
-			Dataset<Row> positives, double sampleFraction, long blockSize,
+    public static Tree<Canopy> createBlockingTree(DataFrame testData,  
+			DataFrame positives, double sampleFraction, long blockSize,
             Arguments args,
             ListMap<DataType, HashFunction> hashFunctions) throws Exception {
-		Dataset<Row> sample = testData.sample(false, sampleFraction);
-		sample = sample.persist(StorageLevel.MEMORY_ONLY());
+		DataFrame sample = testData.sample(sampleFraction);
+		//sample = sample.persist(StorageLevel.MEMORY_ONLY());
 		long totalCount = sample.count();
 		if (LOG.isDebugEnabled()) {
 			LOG.debug("Learning blocking rules for sample count " + totalCount  
@@ -46,9 +43,8 @@ public class BlockingTreeUtil {
 		if (blockSize == -1) blockSize = Heuristics.getMaxBlockSize(totalCount);
 		LOG.info("Learning indexing rules for block size " + blockSize);
        
-		positives = positives.coalesce(1); 
 		Block cblock = new Block(sample, positives, hashFunctions, blockSize);
-		Canopy root = new Canopy(sample.collectAsList(), positives.collectAsList());
+		Canopy root = new Canopy(Arrays.asList(sample.collect()), Arrays.asList(positives.collect()));
 
 		List<FieldDefinition> fd = new ArrayList<FieldDefinition> ();
 
@@ -69,26 +65,25 @@ public class BlockingTreeUtil {
 	}
 
 	
-	public static Tree<Canopy> createBlockingTreeFromSample(Dataset<Row> testData,  
-			Dataset<Row> positives, double sampleFraction, long blockSize, Arguments args, 
+	public static Tree<Canopy> createBlockingTreeFromSample(DataFrame testData,  
+			DataFrame positives, double sampleFraction, long blockSize, Arguments args, 
             ListMap<DataType, HashFunction> hashFunctions) throws Exception {
-		Dataset<Row> sample = testData.sample(false, sampleFraction); 
+		DataFrame sample = testData.sample(sampleFraction); 
 		return createBlockingTree(sample, positives, sampleFraction, blockSize, args, hashFunctions);
 	}
 	
-	public static void writeBlockingTree(SparkSession spark, JavaSparkContext ctx, Tree<Canopy> blockingTree, Arguments args) throws Exception {
+	public static void writeBlockingTree(Session snow, Tree<Canopy> blockingTree, Arguments args) throws Exception {
 		byte[] byteArray  = Util.convertObjectIntoByteArray(blockingTree);
-		StructType schema = DataTypes.createStructType(new StructField[] { DataTypes.createStructField("BlockingTree", DataTypes.BinaryType, false) });
+		StructType schema = StructType.create(new StructField[] { new StructField("BlockingTree", DataTypes.BinaryType, false) });
 		List<Object> objList = new ArrayList<>();
 		objList.add(byteArray);
-		JavaRDD<Row> rowRDD = ctx.parallelize(objList).map((Object row) -> RowFactory.create(row));
-		Dataset<Row> df = spark.sqlContext().createDataFrame(rowRDD, schema).toDF().coalesce(1);
-		PipeUtil.write(df, args, ctx, PipeUtil.getBlockingTreePipe(args));
+		DataFrame df = snow.createDataFrame(objList.toArray(Row[]::new), schema).toDF();
+		PipeUtil.write(df, args, PipeUtil.getBlockingTreePipe(args));
 	}
 
-	public static Tree<Canopy> readBlockingTree(SparkSession spark, Arguments args) throws Exception {
-		Dataset<Row> tree = PipeUtil.read(spark, false, args.getNumPartitions(), false, PipeUtil.getBlockingTreePipe(args));
-		byte [] byteArrayBack = (byte[]) tree.head().get(0);
+	public static Tree<Canopy> readBlockingTree(Session snow, Arguments args) throws Exception {
+		DataFrame tree = PipeUtil.read(snow, false, args.getNumPartitions(), false, PipeUtil.getBlockingTreePipe(args));
+		byte [] byteArrayBack = (byte[]) tree.first().get().get(0);
 		Tree<Canopy> blockingTree = null;
 		blockingTree =  (Tree<Canopy>) Util.revertObjectFromByteArray(byteArrayBack);
 		return blockingTree;
