bb5832ce1ad9ff2640ec27635837af7e8f5ae945
Navin Singh <navin.rathore@gmail.com>
2022-05-11 22:59:37 +0530

    restored EMAIL, LICENCE in zingg.sh for future

--- scripts/zingg.sh
+++ scripts/zingg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #ZINGG_HOME=./assembly/target
 ZINGG_JARS=$ZINGG_HOME/zingg-0.3.3-SNAPSHOT.jar
+EMAIL=xxx@yyy.com
+LICENSE="test"
 
 if [[ -z "${ZINGG_EXTRA_JARS}" ]]; then
   OPTION_JARS=""
@@ -14,4 +16,4 @@ else
   OPTION_SPARK_CONF="${ZINGG_EXTRA_SPARK_CONF}"
 fi
 
-$SPARK_HOME/bin/spark-submit --master $SPARK_MASTER $OPTION_JARS $OPTION_SPARK_CONF --conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.default.parallelism="8" --conf spark.executor.extraJavaOptions="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError -Xloggc:/tmp/memLog.txt -XX:+UseCompressedOops" --conf spark.executor.memory=10g --conf spark.debug.maxToStringFields=200 --driver-class-path $ZINGG_JARS --class zingg.client.Client $ZINGG_JARS $@ 
+$SPARK_HOME/bin/spark-submit --master $SPARK_MASTER $OPTION_JARS $OPTION_SPARK_CONF --conf spark.serializer=org.apache.spark.serializer.KryoSerializer --conf spark.default.parallelism="8" --conf spark.executor.extraJavaOptions="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+HeapDumpOnOutOfMemoryError -Xloggc:/tmp/memLog.txt -XX:+UseCompressedOops" --conf spark.executor.memory=10g --conf spark.debug.maxToStringFields=200 --driver-class-path $ZINGG_JARS --class zingg.client.Client $ZINGG_JARS $@ --email $EMAIL --license $LICENSE 
