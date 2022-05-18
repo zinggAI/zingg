4ecadfb98346214577ee7bd9bb3dc67ce6732591
Navin Singh <navin.rathore@gmail.com>
2022-05-18 17:52:05 +0530

    var renamed to blockSize

--- core/src/test/java/zingg/util/TestHeuristics.java
+++ core/src/test/java/zingg/util/TestHeuristics.java
@@ -5,30 +5,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class TestHeuristics {
-	long maxBlockSizeConfigured = 50L;
+	long blockSizeConfigured = 50L;
 
 	@Test
 	public void testMaxBlockSizeWhenCalculatedValueIsMoreThanConfigValue() throws Throwable {
-		long size = Heuristics.getMaxBlockSize(70000, maxBlockSizeConfigured);
+		long size = Heuristics.getMaxBlockSize(70000, blockSizeConfigured);
 		assertEquals(size, 50);
 	}
 
 	@Test
 	public void testMaxBlockSizeWhenCalculatedValueIsLessThanConfigValueButMoreThanMinValue() throws Throwable {
-		long size = Heuristics.getMaxBlockSize(25000, maxBlockSizeConfigured);
+		long size = Heuristics.getMaxBlockSize(25000, blockSizeConfigured);
 		assertEquals(size, 25);
 	}
 
 	@Test
 	public void testMaxBlockSizeWhenCalculatedValueIsLessThanMinValue() throws Throwable {
-		long size = Heuristics.getMaxBlockSize(5000, maxBlockSizeConfigured);
+		long size = Heuristics.getMaxBlockSize(5000, blockSizeConfigured);
 		assertEquals(size, Heuristics.MIN_SIZE);
 	}
 
 	@Test
 	public void testMaxBlockSizeWhenConfigValueItselfIsLessThanMinValue() throws Throwable {
-		long maxBlockSizeConfigured = 5L;
-		long size = Heuristics.getMaxBlockSize(25000, maxBlockSizeConfigured);
+		long blockSizeConfigured = 5L;
+		long size = Heuristics.getMaxBlockSize(25000, blockSizeConfigured);
 		assertEquals(size, Heuristics.MIN_SIZE);
 	}
 }
