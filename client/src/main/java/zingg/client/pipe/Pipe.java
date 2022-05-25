fe99b05c87101dd272cdacef680d86f755e300b4
chetan <chetan07122000@gmail.com>
2022-05-25 16:38:01 +0530

    moved getRecords & setRecords from InMemoryPipe to Pipe

--- client/src/main/java/zingg/client/pipe/Pipe.java
+++ client/src/main/java/zingg/client/pipe/Pipe.java
@@ -5,11 +5,13 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.types.DataType;
 import org.apache.spark.sql.types.StructType;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonValue;
+import org.apache.spark.sql.Row;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -39,7 +41,8 @@ public class Pipe implements Serializable{
 	Map<String, String> addProps;
 	SaveMode mode;
 	int id;
-	
+	Dataset <Row> dataset;
+
 	public SaveMode getMode() {
 		return mode;
 	}
@@ -144,6 +147,13 @@ public class Pipe implements Serializable{
 		this.id = recId;
 	}
 
+	public Dataset <Row> getRecords(){
+		return this.dataset;
+	}
+
+	public void setRecords(Dataset <Row> ds){
+		this.dataset = ds;
+	}
 
 	@Override
 	public String toString() {
@@ -194,8 +204,8 @@ public class Pipe implements Serializable{
 		p.addProps = addProps;
 		p.mode = mode;
 		p.id = id;
+		p.dataset = dataset;
 		return p;
 	}
-	
 	
 }
