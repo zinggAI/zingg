6e43430025d5149a0737d05d3f79267f7fb5eb50
Navin Singh <navin.rathore@gmail.com>
2022-05-26 20:06:42 +0530

    refactor code and added test cases

--- core/src/main/java/zingg/documenter/ModelDocumenter.java
+++ core/src/main/java/zingg/documenter/ModelDocumenter.java
@@ -24,7 +24,7 @@ public class ModelDocumenter extends DocumenterBase {
 
 	private final String MODEL_TEMPLATE = "model.ftlh";
 	ModelColDocumenter modelColDoc;
-	private Dataset<Row> markedRecords;
+	protected Dataset<Row> markedRecords;
 
 	public ModelDocumenter(SparkSession spark, Arguments args) {
 		super(spark, args);
@@ -47,36 +47,45 @@ public class ModelDocumenter extends DocumenterBase {
 				LOG.warn("No marked record has been found");
 			}
 
-			/* Create a data-model */
-			Map<String, Object> root = new HashMap<String, Object>();
-			root.put(TemplateFields.MODEL_ID, args.getModelId());
-			if(!markedRecords.isEmpty()) {
-				markedRecords = markedRecords.cache();
-
-				root.put(TemplateFields.CLUSTERS, markedRecords.collectAsList());
-				root.put(TemplateFields.NUM_COLUMNS, markedRecords.columns().length);
-				root.put(TemplateFields.COLUMNS, markedRecords.columns());
-				root.put(TemplateFields.ISMATCH_COLUMN_INDEX,
-						markedRecords.schema().fieldIndex(ColName.MATCH_FLAG_COL));
-				root.put(TemplateFields.CLUSTER_COLUMN_INDEX,
-						markedRecords.schema().fieldIndex(ColName.CLUSTER_COLUMN));
-			} else {
-				// fields required to generate basic document
-				List<String> columnList = args.getFieldDefinition().stream().map(fd -> fd.getFieldName())
-						.collect(Collectors.toList());
-				root.put(TemplateFields.NUM_COLUMNS, columnList.size());
-				root.put(TemplateFields.COLUMNS, columnList.toArray());
-				root.put(TemplateFields.CLUSTERS, Collections.emptyList());
-				root.put(TemplateFields.ISMATCH_COLUMN_INDEX, 0);
-				root.put(TemplateFields.CLUSTER_COLUMN_INDEX, 1);
-			}
-			checkAndCreateDir(args.getZinggDocDir());
-			writeDocument(MODEL_TEMPLATE, root, args.getZinggModelDocFile());
+			Map<String, Object> root = populateTemplateData();
+			writeModelDocument(root);
 
 			LOG.info("Model document generation finishes");
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new ZinggClientException(e.getMessage());
 		}
+	}
+
+	private void writeModelDocument(Map<String, Object> root) throws ZinggClientException {
+		checkAndCreateDir(args.getZinggDocDir());
+		writeDocument(MODEL_TEMPLATE, root, args.getZinggModelDocFile());
+	}
+
+	protected Map<String, Object> populateTemplateData() {
+		/* Create a data-model */
+		Map<String, Object> root = new HashMap<String, Object>();
+		root.put(TemplateFields.MODEL_ID, args.getModelId());
+		if(!markedRecords.isEmpty()) {
+			markedRecords = markedRecords.cache();
+
+			root.put(TemplateFields.CLUSTERS, markedRecords.collectAsList());
+			root.put(TemplateFields.NUM_COLUMNS, markedRecords.columns().length);
+			root.put(TemplateFields.COLUMNS, markedRecords.columns());
+			root.put(TemplateFields.ISMATCH_COLUMN_INDEX,
+					markedRecords.schema().fieldIndex(ColName.MATCH_FLAG_COL));
+			root.put(TemplateFields.CLUSTER_COLUMN_INDEX,
+					markedRecords.schema().fieldIndex(ColName.CLUSTER_COLUMN));
+		} else {
+			// fields required to generate basic document
+			List<String> columnList = args.getFieldDefinition().stream().map(fd -> fd.getFieldName())
+					.collect(Collectors.toList());
+			root.put(TemplateFields.NUM_COLUMNS, columnList.size());
+			root.put(TemplateFields.COLUMNS, columnList.toArray());
+			root.put(TemplateFields.CLUSTERS, Collections.emptyList());
+			root.put(TemplateFields.ISMATCH_COLUMN_INDEX, 0);
+			root.put(TemplateFields.CLUSTER_COLUMN_INDEX, 1);
+		}
+		return root;
 	}
 }
--- core/src/test/java/zingg/documenter/TestDataDocumenter.java
+++ core/src/test/java/zingg/documenter/TestDataDocumenter.java
@@ -1,17 +1,17 @@
 package zingg.documenter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -44,12 +44,25 @@ public class TestDataDocumenter extends ZinggSparkTester {
 		}
 		
 		DataDocumenter dataDoc = new DataDocumenter(spark, args);
-		Field f = DataDocumenter.class.getDeclaredField("data");
-		f.setAccessible(true);
-		Dataset<Row> data = PipeUtil.read(spark, false, false, args.getData());
-		f.set(dataDoc, data);
+		dataDoc.data = PipeUtil.read(spark, false, false, args.getData());
 		dataDoc.createDataDocument();
 
 		assertTrue(Files.exists(Paths.get(args.getZinggDataDocFile())), "Data documentation file is not generated");
+	}
+
+	@DisplayName ("Test template data")
+	@Test
+	public void testPopulateTemplateData() throws Throwable {
+		DataDocumenter dataDoc = new DataDocumenter(spark, args);
+		dataDoc.data = PipeUtil.read(spark, false, false, args.getData());
+
+		Map<String, Object> root =  dataDoc.populateTemplateData();
+		assertTrue(root.containsKey(TemplateFields.TITLE), "The field does not exist - " + TemplateFields.TITLE);
+		assertEquals(DataDocumenter.TEMPLATE_TITLE, root.get(TemplateFields.TITLE));
+
+		assertTrue(root.containsKey(TemplateFields.MODEL_ID), "The field does not exist - " + TemplateFields.MODEL_ID);
+		assertEquals(args.getModelId(), root.get(TemplateFields.MODEL_ID));
+
+		assertTrue(root.containsKey(TemplateFields.DATA_FIELDS_LIST), "The field does not exist. - " + TemplateFields.DATA_FIELDS_LIST);
 	}
 }
