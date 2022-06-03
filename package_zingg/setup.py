5531e6df9688bb49c3dfd18de75e8187d9975bcc
Raviraj Baraiya <ravibaraiya2240@gmail.com>
2022-06-03 11:20:44 +0530

    modified setup.py

--- package_zingg/setup.py
+++ package_zingg/setup.py
@@ -4,18 +4,18 @@ setup(
     name='zingg',  
     version='0.0.2',
     include_package_data=True,
-    # packages=['zingg_jar'],
     scripts=['zingg.py'] ,
-    author="Sonal Goyal",
+    author="Zingg.AI",
     author_email="sonalgoyal4@gmail.com",
     description="Zingg.ai environment package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     # url="git_repo_link",
     packages=find_packages(),
+    # packages=['zingg_jar', .. ],
     # py_modules=['module1', ..],
     # install_requires=[
-    #    'requests', 'click', 'configparser'
+    #    'python 3.6+', 'spark 3.1.2', 'configparser', ..
     # ],
     classifiers=[
         "Programming Language :: Python :: 3",
