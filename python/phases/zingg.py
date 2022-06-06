075ed097b0e4c096d5b24a9a99781a5f58bde13b
Navin Singh <navin.rathore@gmail.com>
2022-06-07 01:38:18 +0530

    Added Python classes - MatchType, ClientOptions Added missing Functionality - Arguments, FieldDefinition Introduced python dependency file requirements.txt Updated assessModel script for confusion matrix

--- python/phases/zingg.py
+++ python/phases/zingg.py
@@ -1,4 +1,6 @@
 import logging
+import pandas as pd
+from pyspark.sql import DataFrame
 
 from pyspark import SparkConf, SparkContext, SQLContext
 from pyspark.sql.session import SparkSession
@@ -10,20 +12,42 @@ spark = SparkSession(sc)
 jvm = sc._jvm
 gateway = sc._gateway
 
-class Client:
-    
+class Zingg:
+
     def __init__(self, args, options):
-        self.client = jvm.zingg.client.Client(args.getArgs(), options)
-    
+        self.client = jvm.zingg.client.Client(args.getArgs(), options.getClientOptions())
+
     def init(self):
         self.client.init()
     def execute(self):
         self.client.execute()
     def getMarkedRecords(self):
         return self.client.getMarkedRecords()
-        
+    def getUnMarkedRecords(self):
+        return self.client.getUnMarkedRecords()
+    def setArguments(self, args):
+        self.client.setArguments()
+    def getArguments(self):
+        return self.client.getArguments()
+    def getOptions(self):
+        return self.client.getOptions()
+    def setOptions(self, options):
+        return self.client.setOptions(options)
+    def getMarkedRecordsStat(self, markedRecords, value):
+        return self.client.getMarkedRecordsStat(markedRecords, value)
+    def getMatchedMarkedRecordsStat(self, markedRecords):
+        return self.client.getMatchedMarkedRecordsStat(markedRecords)
+    def getUnmatchedMarkedRecordsStat(self, markedRecords):
+        return self.client.getUnmatchedMarkedRecordsStat(markedRecords)
+    def getUnsureMarkedRecordsStat(self, markedRecords):
+        return self.client.getUnsureMarkedRecordsStat(markedRecords)
+    def getDfFromDs(self, data):
+        return DataFrame(data, sqlContext)
+    def getPandasDfFromDs(self, data):
+        return self.getDfFromDs(data).toPandas()
+
 class Arguments:
-    
+
     def __init__(self):
         self.args = jvm.zingg.client.Arguments()
     def setFieldDefinition(self, fieldDef):
@@ -32,40 +56,69 @@ class Arguments:
         for f in fieldDef:
             javaFieldDef.append(f.getFieldDefinition())
         self.args.setFieldDefinition(javaFieldDef)
-        
+
     def getArgs(self):
         return self.args
-    
+
     def setData(self, pipe):
         dataPipe = gateway.new_array(jvm.zingg.client.pipe.Pipe, 1)
         dataPipe[0] = pipe.getPipe()
         self.args.setData(dataPipe)
-    
-    ## TODO as setData
+
     def setOutput(self, pipe):
-        self.args.setOutput([pipe.getPipe()])
-    
+        outputPipe = gateway.new_array(jvm.zingg.client.pipe.Pipe, 1)
+        outputPipe[0] = pipe.getPipe()
+        self.args.setOutput(outputPipe)
+
     def setModelId(self, id):
         self.args.setModelId(id)
     def setZinggDir(self, f):
         self.args.setZinggDir(f)
-    
+
     def setNumPartitions(self, numPartitions):
         self.args.setNumPartitions(numPartitions)
-    
+
     def setLabelDataSampleSize(self, labelDataSampleSize):
         self.args.setLabelDataSampleSize(labelDataSampleSize)
+
+    @staticmethod
+    def writeArgumentsToJSON(fileName, args):
+        jvm.zingg.client.Arguments.writeArgumentsToJSON(fileName, args)
+
+class ClientOptions:
+
+    def __init__(self, phase="label", conf="dummy"):
+        self.co = sc._jvm.zingg.client.ClientOptions(["--phase", phase, "--conf", conf, "--license", "dummy", "--email", "xxx@yyy.com"])
+
+    def getClientOptions(self):
+        return self.co
+
+    def getPhase(self):
+        return self.co.get("--phase").getValue()
+
+    def getConf(self):
+        return self.co.get("--conf").getValue()
+
 class FieldDefinition:
-    
-    def __init__(self, name, dataType, matchType):
+    def __init__(self, name, dataType, *matchType):
         self.fd = jvm.zingg.client.FieldDefinition()
         self.fd.setFieldName(name)
-        self.fd.setDataType(dataType)
+        self.fd.setDataType(self.stringify(dataType))
         self.fd.setMatchType(matchType)
         self.fd.setFields(name)
-    
+
     def getFieldDefinition(self):
         return self.fd
+
+    # dataType should be stringify'ed before it is set in fd object
+    def stringify(self, str):
+        return '"' + str + '"'
+
+class MatchType:
+    @staticmethod
+    def type(type):
+        return jvm.zingg.client.MatchType.getMatchType(type)
+
 class Pipe:
     def __init__(self, name, format):
         self.pipe = sc._jvm.zingg.client.pipe.Pipe()
@@ -73,6 +126,6 @@ class Pipe:
         self.pipe.setFormat(sc._jvm.zingg.client.pipe.Format.getPipeType(format))
     def getPipe(self):
         return self.pipe
-    
+
     def addProperty(self, name, value):
         self.pipe.setProp(name, value)
