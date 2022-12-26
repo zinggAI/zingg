c93c995b6333e9524dcdf1b468daf5311a133f35
Sonal Goyal <sonalgoyal4@gmail.com>
2022-12-26 21:39:38 +0530

    rm wrong parameterization

--- core/src/main/java/zingg/spark/hash/SparkFirst3CharsBox.java
+++ core/src/main/java/zingg/spark/hash/SparkFirst3CharsBox.java
@@ -11,7 +11,7 @@ import org.apache.spark.sql.types.DataType;
 import zingg.client.ZFrame;
 import zingg.hash.First3CharsBox;
 
-public class SparkFirst3CharsBox<D,R,C,T> extends First3CharsBox<Dataset<Row>,Row,Column,DataType> implements UDF1<String, Integer>{
+public class SparkFirst3CharsBox extends First3CharsBox<Dataset<Row>,Row,Column,DataType> implements UDF1<String, Integer>{
 
 	public SparkFirst3CharsBox() {
 		super();
