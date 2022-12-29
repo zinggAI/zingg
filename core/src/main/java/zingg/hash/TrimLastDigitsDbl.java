7fafaeeff84b1627775fb1c11b61f6c5acdbcc34
Vikas Gupta <vikasgupta78@gmail.com>
2022-12-29 17:49:35 +0530

    changes for hash function

--- core/src/main/java/zingg/hash/TrimLastDigitsDbl.java
+++ core/src/main/java/zingg/hash/TrimLastDigitsDbl.java
@@ -1,20 +1,23 @@
 package zingg.hash;
 
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.api.java.UDF1;
-import org.apache.spark.sql.types.DataTypes;
-
-import zingg.client.ZFrame;
-
-public class TrimLastDigitsDbl extends HashFunction implements UDF1<Double, Double> {
+/**
+ * Base class for hash functions related to trimming of doubles
+ * @author vikasgupta
+ *
+ * @param <D>
+ * @param <R>
+ * @param <C>
+ * @param <T>
+ */
+public abstract class TrimLastDigitsDbl<D,R,C,T> extends HashFunction<D,R,C,T> {
 	int numDigits;
 	static final int[] POWERS_OF_10 = {1, 10, 100, 1000, 10000, 100000};
 	public TrimLastDigitsDbl(int count) {
-		super("trimLast" + count + "DigitsDbl", DataTypes.DoubleType, DataTypes.DoubleType, true);
+		super("trimLast" + count + "DigitsDbl");
 		this.numDigits = count;
 	}
 
-	@Override
+	
 	public Double call(Double field) {
 		Double r = null;
 		if (field == null) {
@@ -23,40 +26,6 @@ public class TrimLastDigitsDbl extends HashFunction implements UDF1<Double, Doub
 			r = Math.floor(field / POWERS_OF_10[numDigits]);
 		}
 		return r;
-	}
-
-	public Object apply(Row ds, String column) {
-		return call((Double) ds.getAs(column));
-	}
-
-	@Override
-	public ZFrame apply(ZFrame ds, String column, String newColumn) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object getAs(Object r, String column) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object getAs(Object df, Object r, String column) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object apply(Object r, String column) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object apply(Object df, Object r, String column) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 	
 }
