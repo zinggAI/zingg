e1156c2dee72e64c6d50b6eb561790aaae75bf42
Vikas Gupta <vikasgupta78@gmail.com>
2022-12-30 13:05:09 +0530

    renamed

--- core/src/main/java/zingg/spark/hash/SparkRangeBetween0And10Int.java
+++ core/src/main/java/zingg/spark/hash/SparkRangeBetween0And10Int.java
@@ -5,9 +5,9 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.types.DataType;
 
-public abstract class SparkRangeBetween0And10DInt extends SparkRangeInt<Dataset<Row>,Row,Column,DataType> {
+public abstract class SparkRangeBetween0And10Int extends SparkRangeInt<Dataset<Row>,Row,Column,DataType> {
 
-	public SparkRangeBetween0And10DInt() {
+	public SparkRangeBetween0And10Int() {
 		super(0, 10);
 	}
 
