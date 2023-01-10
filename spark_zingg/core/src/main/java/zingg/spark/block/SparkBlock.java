e2e88061e57a6330c79cfc56b4c69e1e8ab7cbd3
Vikas Gupta <vikasgupta78@gmail.com>
2023-01-10 13:08:57 +0530

    removed snow import

--- spark_zingg/core/src/main/java/zingg/spark/block/SparkBlock.java
+++ spark_zingg/core/src/main/java/zingg/spark/block/SparkBlock.java
@@ -1,12 +1,10 @@
 package zingg.spark.block;
 
-import org.apache.spark.internal.config.R;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.types.DataType;
 
-import net.snowflake.client.jdbc.internal.org.checkerframework.checker.units.qual.C;
 import zingg.block.Block;
 import zingg.client.ZFrame;
 import zingg.client.util.ListMap;
