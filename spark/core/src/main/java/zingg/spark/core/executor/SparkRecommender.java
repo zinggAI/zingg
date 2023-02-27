ba46db47e97eae0224cbd76699f8cea13d04d662
Vikas Gupta <vikasgupta78@gmail.com>
2023-02-27 16:43:23 +0530

    1st cut for recommender to work

---
+++ spark/core/src/main/java/zingg/spark/core/executor/SparkRecommender.java
@@ -0,0 +1,47 @@
+package zingg.spark.core.executor;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataType;
+
+import zingg.common.core.executor.Recommender;
+import zingg.common.core.executor.Trainer;
+import zingg.common.client.Arguments;
+import zingg.common.client.ZinggClientException;
+import zingg.common.client.ZinggOptions;
+
+
+/**
+ * Spark specific implementation of Recommender
+ * 
+ * @author vikasgupta
+ *
+ */
+public class SparkRecommender extends Recommender<SparkSession, Dataset<Row>, Row, Column,DataType> {
+
+	private static final long serialVersionUID = 1L;
+	public static String name = "zingg.spark.SparkRecommender";
+	public static final Log LOG = LogFactory.getLog(SparkRecommender.class);
+
+	public SparkRecommender() {
+		setZinggOptions(ZinggOptions.RECOMMEND);
+		setContext(new ZinggSparkContext());
+	}
+
+    @Override
+    public void init(Arguments args, String license)  throws ZinggClientException {
+        super.init(args, license);
+        getContext().init(license);
+    }	
+	
+	@Override
+	public void cleanup() throws ZinggClientException {
+		// TODO Auto-generated method stub
+		
+	}		
+	
+}
