08af7c3fc8ff7a1b9c9a2bbfc3002350f6f4d848
Vikas Gupta <vikasgupta78@gmail.com>
2023-03-08 09:08:04 +0530

    fixed junit for data documenter

--- spark/core/src/test/java/zingg/common/core/documenter/TestDataDocumenter.java
+++ spark/core/src/test/java/zingg/common/core/documenter/TestDataDocumenter.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
@@ -13,16 +14,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import zingg.common.client.Arguments;
+import zingg.common.client.pipe.FilePipe;
+import zingg.common.client.pipe.Pipe;
 import zingg.spark.core.documenter.SparkDataDocumenter;
 import zingg.spark.core.executor.ZinggSparkTester;
 
 public class TestDataDocumenter extends ZinggSparkTester {
 	public static final Log LOG = LogFactory.getLog(TestDataDocumenter.class);
 
+	private Arguments docArguments = new Arguments();
+	private String ZINGG_HOME =System.getenv("ZINGG_HOME");
 	@BeforeEach
 	public void setUp(){
 		try {
-			args = Arguments.createArgumentsFromJSON(getClass().getResource("/documenter/config.json").getFile());
+			docArguments = Arguments.createArgumentsFromJSON(ZINGG_HOME+"/../../examples/febrl/config.json");
 		} catch (Throwable e) {
 			e.printStackTrace();
 			LOG.info("Unexpected exception received " + e.getMessage());
@@ -30,18 +35,27 @@ public class TestDataDocumenter extends ZinggSparkTester {
 		}
 	}
 
-	@DisplayName ("Test template data")
 	@Test
 	public void testPopulateTemplateData() throws Throwable {
-		DataDocumenter dataDoc = new SparkDataDocumenter(zsCTX, args);
-		dataDoc.data = zsCTX.getPipeUtil().read(false, false, args.getData());
+		
+		DataDocumenter dataDoc = new SparkDataDocumenter(zsCTX, docArguments);
+		Pipe[] dataPipeArr = docArguments.getData();
+		
+		for (int i = 0; i < dataPipeArr.length; i++) {
+			dataPipeArr[i].setProp(FilePipe.LOCATION, ZINGG_HOME+"/../../examples/febrl/test.csv");
+		}
+				
+		dataDoc.data = zsCTX.getPipeUtil().read(false, false, dataPipeArr);
+		
+		
 
 		Map<String, Object> root =  dataDoc.populateTemplateData();
+		
 		assertTrue(root.containsKey(TemplateFields.TITLE), "The field does not exist - " + TemplateFields.TITLE);
 		assertEquals(DataDocumenter.TEMPLATE_TITLE, root.get(TemplateFields.TITLE));
 
 		assertTrue(root.containsKey(TemplateFields.MODEL_ID), "The field does not exist - " + TemplateFields.MODEL_ID);
-		assertEquals(args.getModelId(), root.get(TemplateFields.MODEL_ID));
+		assertEquals(docArguments.getModelId(), root.get(TemplateFields.MODEL_ID));
 
 		assertTrue(root.containsKey(TemplateFields.DATA_FIELDS_LIST), "The field does not exist. - " + TemplateFields.DATA_FIELDS_LIST);
 	}
