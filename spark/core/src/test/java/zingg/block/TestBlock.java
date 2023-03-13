86c5c17cd322a9c7cece3cc272225297965be101
Vikas Gupta <vikasgupta78@gmail.com>
2023-03-13 18:35:06 +0530

    junit for blocking tree util

--- spark/core/src/test/java/zingg/block/TestBlock.java
+++ spark/core/src/test/java/zingg/block/TestBlock.java
@@ -1,45 +1,256 @@
 package zingg.block;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestBlock {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
-	/*
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.junit.jupiter.api.Test;
+
+import zingg.common.client.Arguments;
+import zingg.common.client.FieldDefinition;
+import zingg.common.client.MatchType;
+import zingg.common.client.ZFrame;
+import zingg.common.client.ZinggClientException;
+import zingg.common.core.block.Canopy;
+import zingg.common.core.block.Tree;
+import zingg.spark.client.SparkFrame;
+import zingg.spark.core.executor.ZinggSparkTester;
+import zingg.spark.core.util.SparkBlockingTreeUtil;
+import zingg.spark.core.util.SparkHashUtil;
+
+public class TestBlock  extends ZinggSparkTester {
+	
 	@Test
-	public void testCheckFunctionInNodeExists() {
-		ScriptFunction sf = mock(ScriptFunction.class);
-		FieldDefinition field = new FieldDefinition(1, MatchType.ALPHANUMERIC,
-				DataType.INT);
-		HashFunctionContext context = new HashFunctionContext(field);
-		//Canopy can = new Canopy(null, null, sf, context);
-		/*
-		Block block = new Block(new ArrayList<Tuple>(), new ArrayList<Dupe>());
-		assertTrue(block.checkFunctionInNode(can, 1, sf));
-		assertFalse(block.checkFunctionInNode(can, 5, sf));
-		ScriptFunction sf1 = mock(ScriptFunction.class);
-		assertFalse(block.checkFunctionInNode(can, 1, sf1));
-		*/
-	//}
+	public void testTree() throws Throwable {
+		
+		ZFrame<Dataset<Row>, Row, Column> testData = getTestData();
 
-	/*
-	 * public boolean checkFunctionInNode(Canopy node, int index, RFunction
-	 * function) { if (node.getFunction() != null &&
-	 * node.getFunction().equals(function) && node.getFieldNumber() == index) {
-	 * return true; } return false; }
-	 */
+		ZFrame<Dataset<Row>, Row, Column> posDf = getPosData();
 
-	/*
-	 * public boolean isFunctionUsed(Tree<Canopy> tree, Canopy node, int index,
-	 * RFunction function) { LOG.info("Tree " + tree); LOG.info("Node  " +
-	 * node); LOG.info("Index " + index); LOG.info("Function " + function);
-	 * boolean isUsed = false; if (node == null || tree == null) return false;
-	 * if (checkFunctionInNode(node, index, function)) return true; Tree<Canopy>
-	 * nodeTree = tree.getTree(node); if (nodeTree == null) return false;
-	 * 
-	 * Tree<Canopy> parent = nodeTree.getParent(); Canopy head =
-	 * parent.getHead(); while (head != null) { //check siblings of node for
-	 * (Tree<Canopy> siblings : parent.getSubTrees()) { Canopy sibling =
-	 * siblings.getHead(); if (checkFunctionInNode(sibling, index, function))
-	 * return true; } //check parent of node return isFunctionUsed(tree, head,
-	 * index, function); } return isUsed; }
-	 */
+		Arguments args = getArguments();
+
+		// form tree
+		SparkBlockingTreeUtil blockingTreeUtil = new SparkBlockingTreeUtil(spark, zsCTX.getPipeUtil());
+		SparkHashUtil hashUtil = new SparkHashUtil(spark);
+
+		Tree<Canopy<Row>> blockingTree = blockingTreeUtil.createBlockingTreeFromSample(testData, posDf, 0.5, -1,
+				args, hashUtil.getHashFunctionList());
+				
+		// primary deciding is unique year so identityInteger should have been picked
+		Canopy<Row> head = blockingTree.getHead();
+		assertEquals("identityInteger", head.getFunction().getName());
+				
+	}
+
+	StructType testDataSchema = new StructType(new StructField[] {
+			new StructField("id", DataTypes.IntegerType, false, Metadata.empty()),
+			new StructField("year", DataTypes.IntegerType, false, Metadata.empty()), 
+			new StructField("event", DataTypes.StringType, false, Metadata.empty()),
+			new StructField("comment", DataTypes.StringType, false, Metadata.empty())}
+		);
+	
+	StructType schemaPos = new StructType(new StructField[] {
+			new StructField("id", DataTypes.IntegerType, false, Metadata.empty()),
+			new StructField("year", DataTypes.IntegerType, false, Metadata.empty()), 
+			new StructField("event", DataTypes.StringType, false, Metadata.empty()),
+			new StructField("comment", DataTypes.StringType, false, Metadata.empty()), 
+			new StructField("z_year", DataTypes.IntegerType, false, Metadata.empty()), 
+			new StructField("z_event", DataTypes.StringType, false, Metadata.empty()),
+			new StructField("z_comment", DataTypes.StringType, false, Metadata.empty()),
+			new StructField("z_zid", DataTypes.StringType, false, Metadata.empty())}
+		);
+	
+	
+	
+	
+	private Arguments getArguments() throws ZinggClientException {
+		String configFilePath = getClass().getResource("../../testFebrl/config.json").getFile();
+		
+		Arguments args = Arguments.createArgumentsFromJSON(configFilePath, "trainMatch");
+
+		List<FieldDefinition> fdList = getFieldDefList();
+
+		args.setFieldDefinition(fdList);
+		return args;
+	}
+
+	private List<FieldDefinition> getFieldDefList() {
+		List<FieldDefinition> fdList = new ArrayList<>(4);
+
+		FieldDefinition idFD = new FieldDefinition();
+		idFD.setDataType("\"integer\"");
+		idFD.setFieldName("id");
+		ArrayList<MatchType> matchTypelistId = new ArrayList<MatchType>();
+		matchTypelistId.add(MatchType.DONT_USE);
+		idFD.setMatchType(matchTypelistId);
+		fdList.add(idFD);
+		
+		ArrayList<MatchType> matchTypelistFuzzy = new ArrayList<MatchType>();
+		matchTypelistFuzzy.add(MatchType.FUZZY);
+
+		
+		FieldDefinition yearFD = new FieldDefinition();
+		yearFD.setDataType("\"integer\"");
+		yearFD.setFieldName("year");
+		yearFD.setMatchType(matchTypelistFuzzy);
+		fdList.add(yearFD);
+		
+		FieldDefinition eventFD = new FieldDefinition();
+		eventFD.setDataType("\"string\"");
+		eventFD.setFieldName("event");
+		eventFD.setMatchType(matchTypelistFuzzy);
+		fdList.add(eventFD);
+		
+		FieldDefinition commentFD = new FieldDefinition();
+		commentFD.setDataType("\"string\"");
+		commentFD.setFieldName("comment");
+		commentFD.setMatchType(matchTypelistFuzzy);
+		fdList.add(commentFD);
+		return fdList;
+	}
+	
+	public SparkFrame getTestData() {
+		int row_id = 1;
+		// Create a DataFrame containing test data
+		Row[] data = { 
+				RowFactory.create(row_id++, new Integer(1942), "quit India", "Mahatma Gandhi"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"), 
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disob", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "quit India", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disobidience", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "Quit Bharat", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disobidence", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "quit Hindustan", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JW", "Amritsar"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Dis", "India") ,
+				RowFactory.create(row_id++, new Integer(1942), "quit Nation", "Mahatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama"),
+				RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"), 
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disob", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "quit India", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disobidience", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "Quit Bharat", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disobidence", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "quit Hindustan", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JW", "Amritsar"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Dis", "India") ,
+				RowFactory.create(row_id++, new Integer(1942), "quit Nation", "Mahatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama"),
+				RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"), 
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disob", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "quit India", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disobidience", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "Quit Bharat", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disobidence", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "quit Hindustan", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JW", "Amritsar"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Dis", "India") ,
+				RowFactory.create(row_id++, new Integer(1942), "quit Nation", "Mahatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama"),
+				RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"), 
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disob", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "quit India", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disobidience", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "Quit Bharat", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JallianWala", "Punjab"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Disobidence", "India"),
+				RowFactory.create(row_id++, new Integer(1942), "quit Hindustan", "Mahatma Gandhi"), 
+				RowFactory.create(row_id++, new Integer(1919), "JW", "Amritsar"),
+				RowFactory.create(row_id++, new Integer(1930), "Civil Dis", "India") ,
+				RowFactory.create(row_id++, new Integer(1942), "quit Nation", "Mahatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma"),
+				RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm"), 
+				RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama"),
+				RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma")				
+		};
+		
+		return new SparkFrame(
+				spark.createDataFrame(Arrays.asList(data), 
+				testDataSchema));
+		
+	}
+
+	private SparkFrame getPosData() {
+		int row_id = 1000;
+		// Create positive matching data
+		Row[] posData = { 
+					RowFactory.create(row_id++, new Integer(1942), "quit Nation", "Mahatma",new Integer(1942), "quit Nation", "Mahatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma",new Integer(1942), "quit N", "Mahatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm",new Integer(1942), "quit ", "Mahatm", "1"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama",new Integer(1942), "quit Ntn", "Mahama", "1"),
+					RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma",new Integer(1942), "quit Natin", "Mahaatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma",new Integer(1942), "quit N", "Mahatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm",new Integer(1942), "quit ", "Mahatm", "1"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama",new Integer(1942), "quit Ntn", "Mahama", "1"),
+					RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma",new Integer(1942), "quit Natin", "Mahaatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma",new Integer(1942), "quit N", "Mahatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm",new Integer(1942), "quit ", "Mahatm", "1"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama",new Integer(1942), "quit Ntn", "Mahama", "1"),
+					RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma",new Integer(1942), "quit Natin", "Mahaatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma",new Integer(1942), "quit N", "Mahatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm",new Integer(1942), "quit ", "Mahatm", "1"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama",new Integer(1942), "quit Ntn", "Mahama", "1"),
+					RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma",new Integer(1942), "quit Natin", "Mahaatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma",new Integer(1942), "quit N", "Mahatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm",new Integer(1942), "quit ", "Mahatm", "1"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama",new Integer(1942), "quit Ntn", "Mahama", "1"),
+					RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma",new Integer(1942), "quit Natin", "Mahaatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit N", "Mahatma",new Integer(1942), "quit N", "Mahatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit ", "Mahatm",new Integer(1942), "quit ", "Mahatm", "1"), 
+					RowFactory.create(row_id++, new Integer(1942), "quit Ntn", "Mahama",new Integer(1942), "quit Ntn", "Mahama", "1"),
+					RowFactory.create(row_id++, new Integer(1942), "quit Natin", "Mahaatma",new Integer(1942), "quit Natin", "Mahaatma", "1"),
+					RowFactory.create(row_id++, new Integer(1919), "JallianWal", "Punjb",new Integer(1919), "JallianWal", "Punjb", "2")				
+				};
+		return new SparkFrame(spark.createDataFrame(Arrays.asList(posData), schemaPos));
+	}
+	
+	
 }
