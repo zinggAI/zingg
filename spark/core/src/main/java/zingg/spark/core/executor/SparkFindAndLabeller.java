4ec7019328be8f736796e36cdacb49e6f4d58e25
Vikas Gupta <vikasgupta78@gmail.com>
2023-03-27 19:12:16 +0530

    findAndLabel phase fixing issue #517

--- spark/core/src/main/java/zingg/spark/core/executor/SparkFindAndLabeller.java
+++ spark/core/src/main/java/zingg/spark/core/executor/SparkFindAndLabeller.java
@@ -10,22 +10,29 @@ import org.apache.spark.sql.SparkSession;
 
 import org.apache.spark.sql.types.DataType;
 
+import zingg.common.client.Arguments;
 import zingg.common.client.ZinggClientException;
 import zingg.common.client.ZinggOptions;
 import zingg.common.core.executor.FindAndLabeller;
 
-
-
 public class SparkFindAndLabeller extends FindAndLabeller<SparkSession, Dataset<Row>, Row, Column,DataType> {
 
+	private static final long serialVersionUID = 1L;
 	public static String name = "zingg.spark.core.executor.SparkFindAndLabeller";
 	public static final Log LOG = LogFactory.getLog(SparkFindAndLabeller.class);
 	SparkSession sparkSession;
 
-	
-
 	public SparkFindAndLabeller() {
-		setZinggOptions(ZinggOptions.FIND_AND_LABEL);
+		setZinggOptions(ZinggOptions.FIND_AND_LABEL);		
+		finder = new SparkTrainingDataFinder();
+		setContext(new ZinggSparkContext());
 	}	
 	
+	@Override
+	public void init(Arguments args, String license) throws ZinggClientException {
+		super.init(args, license);
+		getContext().init(license);
+	}	
+	
+
 }
