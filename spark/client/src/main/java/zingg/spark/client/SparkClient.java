2730f3cc6e8a18b48105fc80b0d237e7e160263d
Vikas Gupta <vikasgupta78@gmail.com>
2023-03-27 16:35:04 +0530

    Revert "formatting"
    This reverts commit f0248861b281e0de2094257e1bcf00bb69120800.

--- spark/client/src/main/java/zingg/spark/client/SparkClient.java
+++ spark/client/src/main/java/zingg/spark/client/SparkClient.java
@@ -1,18 +1,14 @@
 package zingg.spark.client;
 
 import java.io.Serializable;
-
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataType;
 
-import zingg.common.client.Arguments;
-import zingg.common.client.Client;
-import zingg.common.client.ClientOptions;
-import zingg.common.client.IZinggFactory;
-import zingg.common.client.ZinggClientException;
+import zingg.common.client.*;
 
 /**
  * This is the main point of interface with the Zingg matching product.
@@ -22,8 +18,6 @@ import zingg.common.client.ZinggClientException;
  */
 public class SparkClient extends Client<SparkSession, Dataset<Row>, Row, Column, DataType>  implements Serializable {
 	
-	private static final long serialVersionUID = 1L;
-
 	public SparkClient(Arguments args, ClientOptions options) throws ZinggClientException {
 		super(args, options);
 		
