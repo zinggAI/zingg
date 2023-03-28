c2f30b342b4559680f4ad819f1f742c8079b66e2
Vikas Gupta <vikasgupta78@gmail.com>
2023-03-28 12:58:33 +0530

    issue #548 bump up release version

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Zingg.AI'
 author = 'Zingg.AI'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.4'
+release = '0.3.5'
 
 
 # -- General configuration ---------------------------------------------------
