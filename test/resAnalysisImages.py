f22f16c89248b2cb87daa6097c53674a84ef2cdb
Vikas Gupta <vikasgupta78@gmail.com>
2023-08-05 11:27:50 +0530

    image result analysis

---
+++ test/resAnalysisImages.py
@@ -0,0 +1,26 @@
+from pyspark.sql.types import *
+import pandas
+
+import pyspark.sql.functions as fn
+from sentence_transformers import SentenceTransformer, util
+import torch
+import pickle
+
+from PIL import Image
+
+// read data from parquet
+res_df = getSparkSession().read.parquet('/tmp/resultSmallImages')
+res_df.count()
+res_df.show()
+
+// get another df having z_cluster count more than 1
+cluster_df = res_df.groupBy('z_cluster').count()
+cluster_df = cluster_df.filter("count > 1")
+cluster_df = cluster_df.sort(fn.desc('count'))
+cluster_df.show()
+
+// write relavant cols to csv for analysis
+res_partial = res_df.select('z_cluster','item_id','brand','item_name','path').sort(fn.desc('item_name'))
+res_partial.show()
+res_partial.write.csv('/tmp/res_analysis_partial')
+// analyse first DF one by one for data
