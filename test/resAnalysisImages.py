1807a8b0b59c360bd8c01922110b35fd101bbba5
Vikas Gupta <vikasgupta78@gmail.com>
2023-08-06 12:14:02 +0530

    write cluster df also

--- test/resAnalysisImages.py
+++ test/resAnalysisImages.py
@@ -8,19 +8,20 @@ import pickle
 
 from PIL import Image
 
-// read data from parquet
+#read data from parquet
 res_df = getSparkSession().read.parquet('/tmp/resultSmallImages')
 res_df.count()
 res_df.show()
 
-// get another df having z_cluster count more than 1
+#get another df having z_cluster count more than 1
 cluster_df = res_df.groupBy('z_cluster').count()
 cluster_df = cluster_df.filter("count > 1")
 cluster_df = cluster_df.sort(fn.desc('count'))
-cluster_df.show()
+cluster_df = cluster_df.select('z_cluster','item_id','brand','item_name','path')
+cluster_df.write.csv('/tmp/res_image_cluster')
 
-// write relavant cols to csv for analysis
+#write relavant cols to csv for analysis
 res_partial = res_df.select('z_cluster','item_id','brand','item_name','path').sort(fn.desc('item_name'))
 res_partial.show()
 res_partial.write.csv('/tmp/res_analysis_partial')
-// analyse first DF one by one for data
+#analyse above DF one by one for data
