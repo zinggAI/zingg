f9eeac3bebb022f463a715bc9d667eadf3e5c3ec
Vikas Gupta <vikasgupta78@gmail.com>
2023-09-04 12:07:48 +0530

    move to constants

---
+++ common/core/src/main/java/zingg/common/core/preprocess/PreprocConstants.java
@@ -0,0 +1,7 @@
+package zingg.common.core.preprocess;
+
+public interface PreprocConstants {
+
+	public static final String STOP_WORDS_PREPROC = "stopWords";
+
+}
--- spark/core/src/main/java/zingg/spark/core/preprocess/SparkPreprocFactory.java
+++ spark/core/src/main/java/zingg/spark/core/preprocess/SparkPreprocFactory.java
@@ -3,16 +3,14 @@ package zingg.spark.core.preprocess;
 import java.util.HashMap;
 
 import zingg.common.core.preprocess.IPreProc;
+import zingg.common.core.preprocess.PreprocConstants;
 import zingg.common.core.preprocess.PreprocFactory;
 
 public class SparkPreprocFactory extends PreprocFactory {
 	
-	//TODO move to a constant class
-	protected static final String STOP_WORDS_PREPROC = "stopWords";
-
 	public SparkPreprocFactory() {
 		this.preprocMap = new HashMap<String, Class<? extends IPreProc>>();
-		preprocMap.put(STOP_WORDS_PREPROC, SparkStopWordsRemover.class);
+		preprocMap.put(PreprocConstants.STOP_WORDS_PREPROC, SparkStopWordsRemover.class);
 	}
 
 }
