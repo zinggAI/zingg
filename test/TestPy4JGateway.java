0b3de0e7f4e53f63cda4d9b7e74b8886fa9f6212
Sonal Goyal <sonalgoyal4@gmail.com>
2023-09-21 14:17:51 +0530

    updated java py4j gateway to work

--- test/TestPy4JGateway.java
+++ test/TestPy4JGateway.java
@@ -2,7 +2,7 @@ import py4j.GatewayServer;
 public class TestPy4JGateway {
 
     public static void main(String[] args) {
-        GatewayServer gatewayServer = new GatewayServer(new StackEntryPoint());
+        GatewayServer gatewayServer = new GatewayServer();
         gatewayServer.start();
         System.out.println("Gateway Server Started");
     }
