05250ff3e871f9176d8078047fc1f3e96ceb5b7c
Sonal Goyal <sonalgoyal4@gmail.com>
2023-09-25 20:48:41 +0530

    py gateway setup

--- test/TestPy4JGateway.java
+++ test/TestPy4JGateway.java
@@ -2,7 +2,7 @@ import py4j.GatewayServer;
 public class TestPy4JGateway {
 
     public static void main(String[] args) {
-        GatewayServer gatewayServer = new GatewayServer();
+        GatewayServer gatewayServer = new GatewayServer(new Object());
         gatewayServer.start();
         System.out.println("Gateway Server Started");
     }
