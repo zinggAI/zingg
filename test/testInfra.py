480f1f5bfc51cb8e032b3ccf8977b0c810932318
gnanaprakash-ravi <gnana1306@gmail.com>
2023-10-04 10:25:58 +0530

    script and unittests

--- test/testInfra.py
+++ test/testInfra.py
@@ -1,9 +1,9 @@
 import unittest
 import sys
 
-#from zingg import *
-#from zingg.client import *
-#from zingg.pipes import *
+from zingg import *
+from zingg.client import *
+from zingg.pipes import *
 
 import subprocess
 from py4j.java_gateway import JavaGateway
@@ -12,10 +12,12 @@ from time import sleep
 from multiprocessing import Process
 from py4j.java_gateway import JavaGateway, GatewayParameters
 
-PY4J_JAVA_PATH='.:../thirdParty/lib//py4j0.10.9.jar:$ZINGG_HOME/common/client/target/zingg-common-client-0.4.0-SNAPSHOT.jar'
+# PY4J_JAVA_PATH='.:../thirdParty/lib//py4j0.10.9.jar:$ZINGG_HOME/common/client/target/zingg-common-client-0.4.0-SNAPSHOT.jar'
+PY4J_JAVA_PATH='.:../thirdParty/lib//py4j0.10.9.jar:../assembly/target/zingg-0.4.0-SNAPSHOT.jar:/opt/spark-3.2.4-bin-hadoop3.2/jars/jackson-databind-2.12.3.jar:/opt/spark-3.2.4-bin-hadoop3.2/jars/jackson-core-2.12.3.jar:/opt/spark-3.2.4-bin-hadoop3.2/jars/jackson-annotations-2.12.3.jar'
+
 def compileGatewayEntry():
     subprocess.call([
-        "javac", "-cp", PY4J_JAVA_PATH,
+        "javac", "-cp", PY4J_JAVA_PATH, "-source", "1.8", "-target", "1.8",
         "TestPy4JGateway.java"])
     
 def startGatewayEntry():
@@ -57,6 +59,9 @@ class MyJavaIntegrationTest(unittest.TestCase):
         self.p = start_example_app_process()
         self.gateway = JavaGateway(
             gateway_parameters=GatewayParameters(auto_convert=True))
+        # self.arguments = self.gateway.jvm.zingg.common.client.Arguments()
+        self.arguments = Arguments()
+        # self.pipes = Pipe("path_to_data.csv", "CsvPipe")
 
     def tearDown(self):
         safe_shutdown(self)
@@ -69,8 +74,91 @@ class MyJavaIntegrationTest(unittest.TestCase):
         try:
             current_time = self.gateway.jvm.System.currentTimeMillis()
             print("Current time from JVM:", current_time)
+            x = self.gateway.jvm.zingg.common.client.Arguments()
+            # print(x)
+            y  = self.gateway.jvm.zingg.common.client.pipe.Pipe()
+            print(y)
         except Py4JNetworkError:
             print("Failed to access the JVM.")
+    
+    def test_setArgsAndGetArgs(self):
+        expected_args = {
+            "zinggDir": "/tmp/zingg",
+            "numPartitions": 10,
+            "labelDataSampleSize": 0.01,
+            "modelId": "1",
+            "jobId": 1,
+            "collectMetrics": True,
+            # "showConcise": False,
+            # "stopWordsCutoff": 0.1,
+            "blockSize": 100
+        }
+
+        self.arguments.setArgs(expected_args)
+        java_args = self.arguments.getArgs()
+
+        self.assertEqual(java_args, expected_args)
+
+    
+    def test_setDataAndGetArgs(self):
+        pipe1 = Pipe("path_to_data1.csv", "CsvPipe")
+        pipe2 = Pipe("path_to_data2.csv", "CsvPipe")
+
+        self.arguments.setData(pipe1, pipe2)
+
+        java_args = self.arguments.getArgs()
+        java_pipes = java_args.getData()
+
+        self.assertEqual(len(java_pipes), 2)
+
+        for python_pipe, java_pipe in zip([pipe1, pipe2], java_pipes):
+            self.assertEqual(python_pipe.getName(), java_pipe.getName())
+            self.assertEqual(python_pipe.getFormat(), java_pipe.getFormat())
+
+    
+    def test_setFieldDefinition(self):
+        field_def_list = [
+            FieldDefinition("field1", "type1"),
+            FieldDefinition("field2", "type2"),
+        ]
+
+        self.arguments.setFieldDefinition(field_def_list)
+
+        java_args = self.arguments.getArgs()
+        # print(java_args)
+
+        java_field_defs = java_args.getFieldDefinition()
+        print(java_field_defs)
+        print(field_def_list)
+
+        self.assertEqual(len(java_field_defs), len(field_def_list))
+        # self.assertEqual(java_field_defs, field_def_list)
+        # self.assertEqual(java_args["zinggDir"], expected_args["zinggDir"])
+        # self.assertEqual(java_args["numPartitions"], expected_args["numPartitions"])
+        
+        for java_field_def, expected_field_def in zip(java_field_defs, field_def_list):
+            self.assertEqual(java_field_def.getFieldName(), expected_field_def.getFieldName())
+            self.assertEqual(java_field_def.getDataType(), expected_field_def.getDataType())
+
+# class TestArguments(unittest.TestCase):
+#     def setUp(self):
+#         self.arguments = Arguments()
+
+#     def test_setArgsAndGetArgs(self):
+#         expected_args = {
+#             "zinggDir": "/tmp/zingg",
+#             "numPartitions": 10,
+#             "labelDataSampleSize": 0.01,
+#             "modelId": "1",
+#             "jobId": 1,
+#             "collectMetrics": True,
+#             "showConcise": False,
+#             "stopWordsCutoff": 0.1,
+#             "blockSize": 100
+#         }
+#         self.arguments.setArgs(expected_args)
+#         java_args = self.arguments.getArgs()
+#         self.assertEqual(java_args, expected_args)
 
 
 if __name__ == '__main__':
