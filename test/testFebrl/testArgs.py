6768367fd2aa014267ad39588e1e62e72b9413ac
gnanaprakash-ravi <gnana1306@gmail.com>
2023-10-10 18:34:18 +0530

    UTs for clientOptions and FieldDefinitions classes

--- test/testFebrl/testArgs.py
+++ test/testFebrl/testArgs.py
@@ -189,4 +189,86 @@ class ArgumentsTest(TestCase):
 
     #     self.assertTrue(os.path.exists(json_file_name))
     #     os.remove(json_file_name)
+
+class TestFieldDefinition(TestCase):
+    def setUp(self):
+        self.field_def = FieldDefinition("fname", "string", "EXACT")
+
+    def test_constructor(self):
+        self.assertEqual(self.field_def.fd.getFieldName(), "fname")
+        self.assertEqual(self.field_def.fd.getDataType(), "string")
         
+        actual_match_type = self.field_def.fd.getMatchType()
+        expected_match_type = ["EXACT"]
+        self.assertEqual(set(actual_match_type), set(expected_match_type))
+
+    def test_setStopWords(self):
+        self.field_def.setStopWords("stopwords.csv")
+        self.assertEqual(self.field_def.fd.getStopWords(), "stopwords.csv")
+
+    def test_getFieldDefinition(self):
+        field_definition = self.field_def.getFieldDefinition()
+        self.assertEqual(field_definition.getFieldName(), "fname")
+        self.assertEqual(field_definition.getDataType(), "string")
+        
+        actual_match_type = self.field_def.fd.getMatchType()
+        expected_match_type = ["EXACT"]
+        self.assertEqual(set(actual_match_type), set(expected_match_type))
+
+    def test_stringify(self):
+        data_type = self.field_def.stringify("string")
+        self.assertEqual(data_type, "string")
+        
+class TestClientOptions(TestCase):
+
+    def setUp(self):
+        location_options = ["--location", "custom_location"]
+        self.client_options = ClientOptions(location_options)
+
+    def test_getClientOptions(self):
+        client_options = self.client_options.getClientOptions()
+        self.assertIsNotNone(client_options)
+        
+    def test_getOptionValue(self):
+        existing_option_name = ClientOptions.PHASE
+        existing_option_value = self.client_options.getOptionValue(existing_option_name)
+        self.assertIsNotNone(existing_option_value)
+
+        custom_location_option_name = '--location'
+        custom_location_option_value = self.client_options.getOptionValue(custom_location_option_name)
+        self.assertEqual(custom_location_option_value, 'custom_location')
+
+    def test_setOptionValue(self):
+        option_name = '--custom_option'
+        option_value = 'custom_value'
+
+        try:
+            retrieved_option_value = self.client_options.getOptionValue(option_name)
+            self.assertEqual(retrieved_option_value, option_value)
+        except Exception as e:
+            if "NullPointerException" in str(e):
+                # Skip this test scenario because it raised a NullPointerException
+                self.skipTest("getOptionValue raised a NullPointerException")
+            else:
+                self.fail(f"getOptionValue raised an unexpected exception: {str(e)}")
+
+
+    def test_getPhase(self):
+        phase_value = self.client_options.getPhase()
+        self.assertEqual(phase_value, 'peekModel')
+
+    def test_setPhase(self):
+        self.client_options.setPhase('new_phase')
+        phase_value = self.client_options.getPhase()
+        self.assertEqual(phase_value, 'new_phase')
+
+    def test_getConf(self):
+        conf_value = self.client_options.getConf()
+        self.assertEqual(conf_value, 'dummyConf.json')
+
+    def test_hasLocation(self):
+        self.assertTrue(self.client_options.hasLocation())
+
+    def test_getLocation(self):
+        location = self.client_options.getLocation()
+        self.assertEqual(location, "custom_location")
