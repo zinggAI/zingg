3885cdf68e4345e326607a5aafc8cea1749608c9
Vikas Gupta <vikasgupta78@gmail.com>
2024-02-19 16:42:01 +0530

    throws ZinggClientException added

--- common/client/src/main/java/zingg/common/client/event/listeners/IEventListener.java
+++ common/client/src/main/java/zingg/common/client/event/listeners/IEventListener.java
@@ -1,10 +1,11 @@
 package zingg.common.client.event.listeners;
 
+import zingg.common.client.ZinggClientException;
 import zingg.common.client.event.events.IEvent;
 
 public class IEventListener {
 
-    public void listen(IEvent event){
+    public void listen(IEvent event)  throws ZinggClientException {
 
     }
 }
