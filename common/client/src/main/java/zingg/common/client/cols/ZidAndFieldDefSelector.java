6e812ae36bd04f283612a42ebab039fb84713b27
gnanaprakash-ravi <gnana1306@gmail.com>
2024-03-04 14:29:59 +0530

    colName.ID_COL constant

--- common/client/src/main/java/zingg/common/client/cols/ZidAndFieldDefSelector.java
+++ common/client/src/main/java/zingg/common/client/cols/ZidAndFieldDefSelector.java
@@ -3,12 +3,14 @@ package zingg.common.client.cols;
 import java.util.Arrays;
 import java.util.List;
 
+import zingg.common.client.util.ColName;
+
 public class ZidAndFieldDefSelector extends SelectedCols {
 
     public ZidAndFieldDefSelector(String[] fieldDefs) {
 
         List<String> fieldDefList = Arrays.asList(fieldDefs);
-        fieldDefList.add(0, "zid");
+        fieldDefList.add(0, ColName.ID_COL);
         setCols(fieldDefList);
     }
 }
