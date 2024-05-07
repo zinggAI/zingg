51ad35d2866e9229d060796779add37416a53282
Vikas Gupta <vikasgupta78@gmail.com>
2024-05-07 16:46:04 +0530

    remove unwanted checked exceptions

--- common/client/src/main/java/zingg/common/client/event/listeners/IEventListener.java
+++ common/client/src/main/java/zingg/common/client/event/listeners/IEventListener.java
@@ -1,11 +1,10 @@
 package zingg.common.client.event.listeners;
 
-import zingg.common.client.ZinggClientException;
 import zingg.common.client.event.events.IEvent;
 
 public class IEventListener {
 
-    public void listen(IEvent event)  throws ZinggClientException {
+    public void listen(IEvent event) {
 
     }
 }
