2416dc0e28c253f46c756a974af54aa09e42288e
sania-16 <gosania16@gmail.com>
2024-07-18 13:43:10 +0530

    new class for toString()

--- common/client/src/main/java/zingg/common/client/Arguments.java
+++ common/client/src/main/java/zingg/common/client/Arguments.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import zingg.common.client.pipe.Pipe;
+import zingg.common.client.util.JsonStringify;
 
 
 /**
@@ -309,18 +310,7 @@ public class Arguments implements Serializable, IArguments {
 	
 	@Override
 	public String toString() {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS,
-				true);
-		//mapper.configure(JsonParser.Feature.FAIL_ON_EMPTY_BEANS, true)
-		try {
-			StringWriter writer = new StringWriter();
-			return mapper.writeValueAsString(this);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
+		return JsonStringify.toString(this);
 	}
 	
 	/**
