9064316654501cf79804da4564c9787ec072f1ec
sania-16 <gosania16@gmail.com>
2024-10-07 12:13:44 +0530

    first draft issue #902

--- common/client/src/main/java/zingg/common/client/options/ZinggOptions.java
+++ common/client/src/main/java/zingg/common/client/options/ZinggOptions.java
@@ -13,6 +13,7 @@ public class ZinggOptions {
     public final static ZinggOption TRAIN_MATCH = new ZinggOption("trainMatch");
     public final static ZinggOption FIND_TRAINING_DATA = new ZinggOption("findTrainingData");
     public final static ZinggOption LABEL = new ZinggOption("label");
+    public final static ZinggOption DEBUG_BLOCKING = new ZinggOption("debugBlocking");
     public final static ZinggOption LINK = new ZinggOption("link");
     public final static ZinggOption GENERATE_DOCS = new ZinggOption("generateDocs");
     public final static ZinggOption RECOMMEND = new ZinggOption("recommend");
