469bc6e5dcc62b229499488a208cb211fad9c1eb
Nitish <nitish@zingg.ai>
2024-10-23 14:22:13 +0530

    reverted back tests

--- spark/core/src/test/java/zingg/common/core/block/TestSparkBlock.java
+++ spark/core/src/test/java/zingg/common/core/block/TestSparkBlock.java
@@ -11,7 +11,6 @@ import zingg.common.client.ZinggClientException;
 import zingg.common.client.util.IWithSession;
 import zingg.common.client.util.WithSession;
 import zingg.spark.client.util.SparkDFObjectUtil;
-import zingg.spark.core.block.blockingTree.SparkOptimizedBlockingTreeBuilder;
 import zingg.spark.core.context.ZinggSparkContext;
 import zingg.spark.core.util.SparkBlockingTreeUtil;
 import zingg.spark.core.util.SparkHashUtil;
@@ -23,7 +22,7 @@ public class TestSparkBlock extends TestBlockBase<SparkSession, Dataset<Row>, Ro
     public static IWithSession<SparkSession> iWithSession = new WithSession<SparkSession>();
 
     public TestSparkBlock(SparkSession sparkSession) throws ZinggClientException {
-        super(new SparkDFObjectUtil(iWithSession), new SparkHashUtil(sparkSession), new SparkBlockingTreeUtil(sparkSession, zsCTX.getPipeUtil(), new SparkOptimizedBlockingTreeBuilder()));
+        super(new SparkDFObjectUtil(iWithSession), new SparkHashUtil(sparkSession), new SparkBlockingTreeUtil(sparkSession, zsCTX.getPipeUtil()));
         iWithSession.setSession(sparkSession);
         zsCTX.init(sparkSession);
     }
