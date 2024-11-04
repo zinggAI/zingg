338a0912058ae15f394f9b8e752f900752234313
sania-16 <gosania16@gmail.com>
2024-11-04 19:32:03 +0530

    linker junits changes

--- common/core/src/test/java/zingg/common/core/executor/LinkerValidator.java
+++ common/core/src/test/java/zingg/common/core/executor/LinkerValidator.java
@@ -29,10 +29,18 @@ public class LinkerValidator<S, D, R, C, T> extends MatcherValidator<S, D, R, C,
 		ZFrame<D, R, C> df2 = getOutputData().distinct().withColumn("z_zsource", "test2");
         df2 = df2.select("z_zsource", getClusterColName());
 					
-		ZFrame<D, R, C> gold = joinAndFilter("fname", df1, df2).cache();
+		ZFrame<D, R, C> gold = joinAndFilter("z_zsource", df1, df2).cache();
 		ZFrame<D, R, C> result = joinAndFilter(getClusterColName(), df1, df2).cache();
 
         testAccuracy(gold, result);	
+	}
+
+	@Override
+	protected ZFrame<D, R, C> joinAndFilter(String colName, ZFrame<D, R, C> df, ZFrame<D, R, C> df1){
+		C col1 = df.col(colName+"1");
+		C col2 = df1.col(colName+"2");
+		ZFrame<D, R, C> joined = df.joinOnCol(df1, df.equalTo(col1, col2));
+		return joined.filter(joined.gt(joined.col("z_zsource1"), joined.col("z_zsource2")));
 	}
     
 }
