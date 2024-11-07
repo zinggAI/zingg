0765f642adb9b7a775d5e348a0c0a83b5892aa6d
Nitish <nitish@zingg.ai>
2024-11-07 15:49:39 +0530

    relative path from repository root taken

--- perf_test_input.py
+++ perf_test_input.py
@@ -5,15 +5,15 @@ MATCH = "match"
 INCREMENTAL = "runIncrement"
 
 #load file config to test on
-LOAD_65 = "/home/administrator/zingg/zinggEnterprise/spark/examples/febrl/config.json"
-LOAD_120k = "/home/administrator/zingg/zinggEnterprise/spark/examples/febrl120k/config120k.json"
+LOAD_65 = "examples/febrl120k/config.json"
+LOAD_120k = "examples/febrl120k/config120k.json"
 
 
 #bash script location
-ZINGG = "/home/administrator/zingg/zinggEnterprise/spark/scripts/zingg.sh"
+ZINGG = "scripts/zingg.sh"
 
 
 #add all the load to test
-load_configs = {LOAD_65 : "LOAD_65"}
+load_configs = {LOAD_65 : "LOAD_65", LOAD_120k: "LOAD_120k"}
 #add all the phases on which testing is required
-phases = [FIND_TRAINING_DATA]
+phases = [FIND_TRAINING_DATA, MATCH]
