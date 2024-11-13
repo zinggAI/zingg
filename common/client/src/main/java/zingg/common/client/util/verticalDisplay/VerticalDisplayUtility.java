50e5d14d6faf9ed9e174f359f9708441648fa175
Nitish <nitish@zingg.ai>
2024-11-13 11:19:46 +0530

    removed getComparisonPairs

--- common/client/src/main/java/zingg/common/client/util/verticalDisplay/VerticalDisplayUtility.java
+++ common/client/src/main/java/zingg/common/client/util/verticalDisplay/VerticalDisplayUtility.java
@@ -3,7 +3,6 @@ package zingg.common.client.util.verticalDisplay;
 import zingg.common.client.ZFrame;
 import zingg.common.client.ZinggClientException;
 import zingg.common.client.util.DFObjectUtil;
-import zingg.common.client.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,37 +24,22 @@ public class VerticalDisplayUtility<S, D, R, C> {
     public ZFrame<D, R, C> convertVertical(ZFrame<D, R, C> zFrame) throws ZinggClientException {
         try {
             String[] columns = zFrame.columns();
-            List<Pair<String, String>> comparison_pairs = getComparisonPairs(zFrame, columns);
-            List<VerticalDisplayModel> rowList = getList(comparison_pairs, columns);
-            return dfObjectUtil.getDFFromObjectList(rowList, VerticalDisplayTwoRowModel.class);
+            ZFrame<D, R, C> zFrame1 = zFrame.limit(1);
+            ZFrame<D, R, C> zFrame2 = zFrame.except(zFrame1);
+
+            R row1 = zFrame.head();
+            R row2 = zFrame2.head();
+            //iterate through all the columns
+            //and create model list
+            List<VerticalDisplayModel> samples = new ArrayList<>();
+            for (String column : columns) {
+                samples.add(new VerticalDisplayTwoRowModel(column, getString(zFrame.getAsString(row1, column)),
+                        getString(zFrame2.getAsString(row2, column))));
+            }
+            return dfObjectUtil.getDFFromObjectList(samples, VerticalDisplayTwoRowModel.class);
         } catch (Exception exception) {
             throw new ZinggClientException("Error occurred while converting to vertical, " + exception.getMessage());
         }
-    }
-
-    private List<VerticalDisplayModel> getList(List<Pair<String, String>> comparison_pairs, String[] columns) {
-        List<VerticalDisplayModel> samples = new ArrayList<>();
-        for (int idx = 0; idx < comparison_pairs.size(); idx++){
-            samples.add(new VerticalDisplayTwoRowModel(columns[idx], comparison_pairs.get(idx).getFirst(), comparison_pairs.get(idx).getSecond()));
-        }
-
-        return samples;
-    }
-
-    private List<Pair<String, String>> getComparisonPairs(ZFrame<D, R, C> zFrame, String[] columns) {
-
-        ZFrame<D, R, C> zFrame1 = zFrame.limit(1);
-        ZFrame<D, R, C> zFrame2 = zFrame.except(zFrame1);
-
-        R row1 = zFrame.head();
-        R row2 = zFrame2.head();
-
-        List<Pair<String, String>> comparison_pairs = new ArrayList<>();
-        for (String column : columns) {
-            comparison_pairs.add(new Pair<String, String>(getString(zFrame.getAsString(row1, column)), getString(zFrame2.getAsString(row2, column))));
-        }
-
-        return comparison_pairs;
     }
 
     private String getString(Object object) {
