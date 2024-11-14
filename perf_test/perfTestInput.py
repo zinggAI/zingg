804c1e457ee8a53e3b1a5b71e6d2bf53ead00b12
Nitish <nitish@zingg.ai>
2024-11-14 18:20:10 +0530

    renamed config names

--- perf_test/perfTestInput.py
+++ perf_test/perfTestInput.py
@@ -5,9 +5,9 @@ MATCH = "match"
 INCREMENTAL = "runIncrement"
 
 #load file config to test on
-config_65 = "./examples/febrl120k/config.json"
-config_120k = "./examples/febrl120k/config120k.json"
-config_5m = "./examples/ncVoters5M/config.json"
+febrl = "./examples/febrl120k/config.json"
+febrl_120k = "./examples/febrl120k/config120k.json"
+ncVoter_5m = "./examples/ncVoters5M/config.json"
 
 
 #bash script location
@@ -15,6 +15,6 @@ ZINGG = "./scripts/zingg.sh"
 
 
 #add all the load to test
-load_configs = {"65_samples" : config_65, "120k_samples" : config_120k, "5m_samples" : config_5m}
+load_configs = {"65_samples" : febrl, "120k_samples" : febrl_120k, "5m_samples" : ncVoter_5m}
 #add all the phases on which testing is required
 phases = [FIND_TRAINING_DATA, MATCH]
