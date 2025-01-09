58bac3a8e48f53130f91b349c59e2f71a6511407
sania-16 <gosania16@gmail.com>
2025-01-09 13:10:38 +0530

    code cleanup

--- common/core/src/main/java/zingg/common/core/preprocess/IPreprocessors.java
+++ common/core/src/main/java/zingg/common/core/preprocess/IPreprocessors.java
@@ -1,7 +1,5 @@
 package zingg.common.core.preprocess;
 
-import java.lang.reflect.InvocationTargetException;
-
 import zingg.common.client.FieldDefinition;
 import zingg.common.client.IArguments;
 import zingg.common.client.IZArgs;
@@ -17,19 +15,24 @@ public interface IPreprocessors<S,D,R,C,T> extends INeedsPreprocMap<S,D,R,C,T>, 
 
     public IZArgs getArgs();
 
-    default ZFrame<D,R,C> preprocess(ZFrame<D,R,C> df) throws InstantiationException, IllegalAccessException, ZinggClientException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException { 
-        ZFrame<D,R,C> dfp = df;  
-        for(FieldDefinition def:((IArguments) getArgs()).getFieldDefinition()){
-            for(IPreprocType o: getPreprocOrder()){
-                //creating new instance of the class
-                IPreprocessor ip = getPreprocMap().get(o).getDeclaredConstructor().newInstance(); 
-                //setting context and field defn
-                ip.setContext(getContext());
-                ip.setFieldDefinition(def);
-                dfp = ip.preprocess(dfp);
-            }
+    default ZFrame<D,R,C> preprocess(ZFrame<D,R,C> df) throws ZinggClientException { 
+        ZFrame<D,R,C> dfp = df; 
+        try{ 
+            for(FieldDefinition def:((IArguments) getArgs()).getFieldDefinition()){
+                for(IPreprocType o: getPreprocOrder()){
+                    //creating new instance of the class
+                    IPreprocessor ip = getPreprocMap().get(o).getDeclaredConstructor().newInstance(); 
+                    //setting context and field defn
+                    ip.setContext(getContext());
+                    ip.setFieldDefinition(def);
+                    dfp = ip.preprocess(dfp);
+                }
+            }  
         }
-        return dfp;  
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return dfp;
     }
 
 }
