dc5b5e69fd0cda3aa9df487d16248344f9846050
sania-16 <gosania16@gmail.com>
2025-01-22 12:44:19 +0530

    code cleanup

--- common/core/src/main/java/zingg/common/core/preprocess/IPreprocessors.java
+++ common/core/src/main/java/zingg/common/core/preprocess/IPreprocessors.java
@@ -29,14 +29,11 @@ public interface IPreprocessors<S,D,R,C,T> extends INeedsPreprocMap<S,D,R,C,T>, 
                 for(IPreprocType o: getPreprocOrder().getOrder()){
                     //creating new instance of the class
                     IPreprocessor<S,D,R,C,T> ip = getPreprocMap().get(o).getDeclaredConstructor().newInstance(); 
-                    LOG.info("trying preproc " + ip);
                     //setting context and field defn
                     ip.setContext(getContext());
                     ip.init();
                     ip.setFieldDefinition(def);
                     dfp = ip.preprocess(dfp);
-                    LOG.info("after preproc ");
-                    dfp.show();
                 }
             }  
         }
