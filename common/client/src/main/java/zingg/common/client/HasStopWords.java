02deb61372888e3f133929dd2c820eba0bac8e8d
sania-16 <gosania16@gmail.com>
2025-02-05 18:32:42 +0530

    pmd comments

--- common/client/src/main/java/zingg/common/client/HasStopWords.java
+++ common/client/src/main/java/zingg/common/client/HasStopWords.java
@@ -5,12 +5,7 @@ import scala.Serializable;
 public class HasStopWords implements Serializable {
 
     public static boolean isStopwordField(FieldDefinition f){
-        if(!(f.getStopWords() == null || f.getStopWords() == "")){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return (!(f.getStopWords() == null || f.getStopWords() == ""));
     }
     
 }
