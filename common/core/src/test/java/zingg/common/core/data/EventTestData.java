4a6b3d2203edbbfd74e32abe60f6710d50d13fb1
Nitish <nitish@zingg.ai>
2025-02-06 18:38:38 +0530

    added junits for case normalizer

--- common/core/src/test/java/zingg/common/core/data/EventTestData.java
+++ common/core/src/test/java/zingg/common/core/data/EventTestData.java
@@ -1,5 +1,6 @@
 package zingg.common.core.data;
 
+import zingg.common.core.model.InputDataModel;
 import zingg.common.core.model.Event;
 import zingg.common.core.model.EventPair;
 import zingg.common.core.model.Statement;
@@ -282,6 +283,71 @@ public class EventTestData {
                 "It written java scala", "four", "", "test"));
         sample.add(new PostStopWordProcess("1648811730857:40", "40", "1.0", "1.0", "-1", "Best luck zingg", "Five",
                 "thank", "test"));
+
+        return sample;
+    }
+
+    public static List<InputDataModel> getDataInputPreCaseNormalization() {
+
+        List<InputDataModel> sample = new ArrayList<InputDataModel>();
+        sample.add(new InputDataModel("10", "The ZINGG IS a SPARK AppLiCation", "tWo",
+                "Yes. a good Application", "test"));
+        sample.add(new InputDataModel("20", "It is VERY POpuLar in Data SCIENCE", "THREE", "TRUE indeed",
+                "test"));
+        sample.add(new InputDataModel("30", "It is WRITTEN in java and SCala", "four", "", "test"));
+        sample.add(new InputDataModel("40", "Best of LUCK to ZINGG Mobile/T-Mobile", "FIVE", "thank you", "test"));
+
+        return sample;
+    }
+
+    public static List<InputDataModel> getDataInputPostCaseNormalizationField1() {
+
+        List<InputDataModel> sample = new ArrayList<InputDataModel>();
+        sample.add(new InputDataModel("10", "the zingg is a spark application", "tWo",
+                "Yes. a good Application", "test"));
+        sample.add(new InputDataModel("20", "it is very popular in data science", "THREE", "TRUE indeed",
+                "test"));
+        sample.add(new InputDataModel("30", "it is written in java and scala", "four", "", "test"));
+        sample.add(new InputDataModel("40", "best of luck to zingg mobile/t-mobile", "FIVE", "thank you", "test"));
+
+        return sample;
+    }
+
+    public static List<InputDataModel> getDataInputPostCaseNormalizationAllFields() {
+
+        List<InputDataModel> sample = new ArrayList<InputDataModel>();
+        sample.add(new InputDataModel("10", "the zingg is a spark application", "two",
+                "yes. a good application", "test"));
+        sample.add(new InputDataModel("20", "it is very popular in data science", "three", "true indeed",
+                "test"));
+        sample.add(new InputDataModel("30", "it is written in java and scala", "four", "", "test"));
+        sample.add(new InputDataModel("40", "best of luck to zingg mobile/t-mobile", "five", "thank you", "test"));
+
+        return sample;
+    }
+
+    public static List<InputDataModel> getDataInputPostCaseNormalizationNone() {
+
+        List<InputDataModel> sample = new ArrayList<InputDataModel>();
+        sample.add(new InputDataModel("10", "The ZINGG IS a SPARK AppLiCation", "tWo",
+                "Yes. a good Application", "test"));
+        sample.add(new InputDataModel("20", "It is VERY POpuLar in Data SCIENCE", "THREE", "TRUE indeed",
+                "test"));
+        sample.add(new InputDataModel("30", "It is WRITTEN in java and SCala", "four", "", "test"));
+        sample.add(new InputDataModel("40", "Best of LUCK to ZINGG Mobile/T-Mobile", "FIVE", "thank you", "test"));
+
+        return sample;
+    }
+
+    public static List<InputDataModel> getDataInputPostCaseNormalizationWhenMatchTypeDONT_USE() {
+
+        List<InputDataModel> sample = new ArrayList<InputDataModel>();
+        sample.add(new InputDataModel("10", "The ZINGG IS a SPARK AppLiCation", "two",
+                "Yes. a good Application", "test"));
+        sample.add(new InputDataModel("20", "It is VERY POpuLar in Data SCIENCE", "three", "TRUE indeed",
+                "test"));
+        sample.add(new InputDataModel("30", "It is WRITTEN in java and SCala", "four", "", "test"));
+        sample.add(new InputDataModel("40", "Best of LUCK to ZINGG Mobile/T-Mobile", "five", "thank you", "test"));
 
         return sample;
     }
