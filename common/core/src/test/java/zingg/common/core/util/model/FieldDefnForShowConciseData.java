621c936ea123b17ff14cca757d9d0285af270711
sania-16 <gosania16@gmail.com>
2025-02-07 18:06:27 +0530

    merge

---
+++ common/core/src/test/java/zingg/common/core/util/model/FieldDefnForShowConciseData.java
@@ -0,0 +1,18 @@
+package zingg.common.core.util.model;
+
+public class FieldDefnForShowConciseData {
+
+    public final String field_fuzzy;
+    public final String field_match_type_DONT_USE;
+    public final String field_str_DONTspaceUSE;
+    public final String z_zsource;
+
+    public FieldDefnForShowConciseData(String field_fuzzy, String field_match_type_DONT_USE, String field_str_DONTspaceUSE, String z_zsource) {
+        this.field_fuzzy = field_fuzzy;
+        this.field_match_type_DONT_USE = field_match_type_DONT_USE;
+        this.field_str_DONTspaceUSE = field_str_DONTspaceUSE;
+        this.z_zsource = z_zsource;
+        
+    }
+    
+}
