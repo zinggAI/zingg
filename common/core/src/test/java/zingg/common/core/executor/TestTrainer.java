822ec87deb6686e3e00b288dc1f68a2c9a2eaf4c
sania-16 <gosania16@gmail.com>
2025-02-10 16:39:16 +0530

    trainer junits

--- common/core/src/test/java/zingg/common/core/executor/TestTrainer.java
+++ common/core/src/test/java/zingg/common/core/executor/TestTrainer.java
@@ -11,37 +11,25 @@ import zingg.common.client.ZinggClientException;
 import zingg.common.core.context.Context;
 import zingg.common.core.util.IDataFrameUtility;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class TestTrainer<S,D,R,C,T> {
     
     protected final Context<S, D, R, C, T> context;
-
+    
     public TestTrainer(Context<S,D,R,C,T> context) {
         this.context = context;
-    }
-    
-    public ZFrame<D,R,C> tenRowsDF;
-    public ZFrame<D,R,C> oneRowsDF;
-
-    @BeforeAll
-    public void setupDF() {
-        tenRowsDF = getDataFrameUtility().createDFWithDoubles(10,1, context.getSession());
-        oneRowsDF = getDataFrameUtility().createDFWithDoubles(1,1, context.getSession());
     }
 
     public abstract Trainer<S,D,R,C,T> getTestTrainer();
 
-    public abstract IDataFrameUtility<S,D,R,C,T> getDataFrameUtility();
+    public abstract ZFrame<D,R,C> getTenRowsDF();
 
-    public abstract ZFrame<D,R,C> getTenRowsDF(ZFrame<D,R,C> tenRowsDF);
-
-    public abstract ZFrame<D,R,C> getOneRowsDF(ZFrame<D,R,C> oneRowsDF);
+    public abstract ZFrame<D,R,C> getOneRowsDF();
 
     @Test
     public void testVerifyTrainingPosDatasetLess() throws Throwable{
         try {
             Trainer<S,D,R,C,T> trainer = getTestTrainer();
-            trainer.verifyTraining(getOneRowsDF(oneRowsDF), getTenRowsDF(tenRowsDF));
+            trainer.verifyTraining(getOneRowsDF(), getTenRowsDF());
             fail("Expected exception not getting thrown when training data is less");
         }
         catch(ZinggClientException e) {
@@ -53,7 +41,7 @@ public abstract class TestTrainer<S,D,R,C,T> {
     public void testVerifyTrainingNegDatasetLess() throws Throwable{
         try {
             Trainer<S,D,R,C,T> trainer = getTestTrainer();
-            trainer.verifyTraining(getTenRowsDF(tenRowsDF), getOneRowsDF(oneRowsDF));
+            trainer.verifyTraining(getTenRowsDF(), getOneRowsDF());
             fail("Expected exception not getting thrown when training data is less");
         }
         catch(ZinggClientException e) {
@@ -65,7 +53,7 @@ public abstract class TestTrainer<S,D,R,C,T> {
     public void testVerifyTrainingBothDatasetLess() throws Throwable{
         try {
             Trainer<S,D,R,C,T> trainer = getTestTrainer();
-            trainer.verifyTraining(getOneRowsDF(oneRowsDF), getOneRowsDF(oneRowsDF));
+            trainer.verifyTraining(getOneRowsDF(), getOneRowsDF());
             fail("Expected exception not getting thrown when training data is less");
         }
         catch(ZinggClientException e) {
@@ -78,7 +66,7 @@ public abstract class TestTrainer<S,D,R,C,T> {
     public void testVerifyTrainingBothDatasetMore() throws Throwable{
         try {
             Trainer<S,D,R,C,T> trainer = getTestTrainer();
-            trainer.verifyTraining(getTenRowsDF(tenRowsDF), getTenRowsDF(tenRowsDF));
+            trainer.verifyTraining(getTenRowsDF(), getTenRowsDF());
             
         }
         catch(ZinggClientException e) {
--- spark/core/src/test/java/zingg/spark/core/executor/TestSparkTrainer.java
+++ spark/core/src/test/java/zingg/spark/core/executor/TestSparkTrainer.java
@@ -5,7 +5,6 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.types.DataType;
-
 import org.junit.jupiter.api.extension.ExtendWith;
 import zingg.common.client.ZFrame;
 import zingg.common.client.ZinggClientException;
@@ -13,8 +12,6 @@ import zingg.common.client.util.IWithSession;
 import zingg.common.client.util.WithSession;
 import zingg.common.core.executor.TestTrainer;
 import zingg.common.core.executor.Trainer;
-import zingg.common.core.util.IDataFrameUtility;
-import zingg.spark.client.SparkFrame;
 import zingg.spark.core.TestSparkBase;
 import zingg.spark.core.context.ZinggSparkContext;
 import zingg.spark.core.util.SparkDataFrameUtility;
@@ -24,11 +21,19 @@ public class TestSparkTrainer extends TestTrainer<SparkSession, Dataset<Row>, Ro
 
     private static ZinggSparkContext zinggSparkContext = new ZinggSparkContext();
     public static IWithSession<SparkSession> iWithSession = new WithSession<SparkSession>();
+    protected ZFrame<Dataset<Row>, Row, Column> tenRowsDF;
+    protected ZFrame<Dataset<Row>, Row, Column> oneRowsDF;
 
     public TestSparkTrainer(SparkSession sparkSession) throws ZinggClientException {
         super(zinggSparkContext); 
         iWithSession.setSession(sparkSession);
 		zinggSparkContext.init(sparkSession); 
+        setupDF();
+    }
+
+    public void setupDF() {
+        tenRowsDF = new SparkDataFrameUtility().createDFWithDoubles(10,1, context.getSession());
+        oneRowsDF = new SparkDataFrameUtility().createDFWithDoubles(1,1, context.getSession());
     }
 
     @Override
@@ -37,18 +42,13 @@ public class TestSparkTrainer extends TestTrainer<SparkSession, Dataset<Row>, Ro
     }
 
     @Override
-    public IDataFrameUtility<SparkSession, Dataset<Row>, Row, Column, DataType> getDataFrameUtility() {
-        return new SparkDataFrameUtility();
+    public ZFrame<Dataset<Row>, Row, Column> getTenRowsDF() {
+       return tenRowsDF;
     }
 
     @Override
-    public ZFrame<Dataset<Row>, Row, Column> getTenRowsDF(ZFrame<Dataset<Row>, Row, Column> tenRowsDF) {
-       return new SparkFrame(tenRowsDF.df());
-    }
-
-    @Override
-    public ZFrame<Dataset<Row>, Row, Column> getOneRowsDF(ZFrame<Dataset<Row>, Row, Column> oneRowsDF) {
-        return new SparkFrame(oneRowsDF.df());
+    public ZFrame<Dataset<Row>, Row, Column> getOneRowsDF() {
+        return oneRowsDF;
     }
 
 }
