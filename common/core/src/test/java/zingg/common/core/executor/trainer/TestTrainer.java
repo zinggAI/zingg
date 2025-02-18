0790141dc456f0ab966db56adb93e9b56bdf9fa3
sania-16 <gosania16@gmail.com>
2025-02-18 16:56:43 +0530

    removing try catch in junits

--- common/core/src/test/java/zingg/common/core/executor/trainer/TestTrainer.java
+++ common/core/src/test/java/zingg/common/core/executor/trainer/TestTrainer.java
@@ -1,6 +1,8 @@
 package zingg.common.core.executor.trainer;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import zingg.common.client.ZFrame;
@@ -35,36 +37,45 @@ public abstract class TestTrainer<S,D,R,C,T> {
     @Test
     public void testVerifyTrainingPosDatasetLess() throws ZinggClientException{
         Trainer<S,D,R,C,T> trainer = getTestTrainer();
-        trainer.verifyTraining(oneRowsDF, tenRowsDF);
-        fail("Expected exception not getting thrown when training data is less");
+        ZinggClientException ex = assertThrows(ZinggClientException.class, 
+            () -> trainer.verifyTraining(oneRowsDF, tenRowsDF),
+            "Expected exception not thrown when positive training data is less");
+        assertTrue(ex.getMessage().contains("insufficient training data"), "Unexpected error message: " + ex.getMessage());
     }
 
     @Test
     public void testVerifyTrainingNegDatasetLess() throws ZinggClientException{
         Trainer<S,D,R,C,T> trainer = getTestTrainer();
-        trainer.verifyTraining(tenRowsDF, oneRowsDF);
-        fail("Expected exception not getting thrown when training data is less");
+        ZinggClientException ex = assertThrows(ZinggClientException.class, 
+            () -> trainer.verifyTraining(tenRowsDF, oneRowsDF),
+            "Expected exception not thrown when negative training data is less");
+        assertTrue(ex.getMessage().contains("insufficient training data"), "Unexpected error message: " + ex.getMessage());
     }
     
     @Test
     public void testVerifyTrainingBothDatasetLess() throws ZinggClientException{
         Trainer<S,D,R,C,T> trainer = getTestTrainer();
-        trainer.verifyTraining(oneRowsDF,oneRowsDF);
-        fail("Expected exception not getting thrown when training data is less");
+        ZinggClientException ex = assertThrows(ZinggClientException.class, 
+            () -> trainer.verifyTraining(oneRowsDF, oneRowsDF),
+            "Expected exception not thrown when both datasets are insufficient");
+        assertTrue(ex.getMessage().contains("insufficient training data"), "Unexpected error message: " + ex.getMessage());
     }
 
     @Test
     public void testVerifyTrainingBothDatasetMore() throws ZinggClientException{
         Trainer<S,D,R,C,T> trainer = getTestTrainer();
-        trainer.verifyTraining(tenRowsDF, tenRowsDF);
-        fail("Exception should not have been thrown when training data is appopriate");
+        // no exception is thrown
+        assertDoesNotThrow(() -> trainer.verifyTraining(tenRowsDF, tenRowsDF),
+            "Exception should not have been thrown when training data is appropriate");
     }
 
     @Test
     public void testVerifyTrainingBothDatasetNull() throws ZinggClientException{
         Trainer<S,D,R,C,T> trainer = getTestTrainer();
-        trainer.verifyTraining(null, null);
-        fail("Expected exception not getting thrown when training data is less");
+        ZinggClientException ex = assertThrows(ZinggClientException.class, 
+            () -> trainer.verifyTraining(null, null),
+            "Expected exception not thrown when both datasets are null");
+        assertTrue(ex.getMessage().contains("insufficient positive training data"), "Unexpected error message: " + ex.getMessage());
     }
 
 }
