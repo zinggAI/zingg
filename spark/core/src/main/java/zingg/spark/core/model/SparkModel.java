493d14de80f654a8b93c036cb67fdd0858cc0255
sania-16 <gosania16@gmail.com>
2025-02-20 12:37:44 +0530

    adding missing import

--- spark/core/src/main/java/zingg/spark/core/model/SparkModel.java
+++ spark/core/src/main/java/zingg/spark/core/model/SparkModel.java
@@ -21,6 +21,7 @@ import org.apache.spark.ml.tuning.ParamGridBuilder;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataType;
 
 import zingg.common.client.FieldDefinition;
