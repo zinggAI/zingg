c6cb1c897af9d9918c6568674bf3d7075c12ea9f
Sania Goyal <85894828+sania-16@users.noreply.github.com>
2025-03-05 12:22:42 +0530

    VerifyBlocking Junit PR (#1071)
    * report generated
    
    * refactoring verifyblocking for tests
    
    * verify blocking junits issue #1034
    
    * verify blocking junits issue #1034
    
    * adding counts for df
    
    * adding tests for output
    
    * adding tests for output check
    
    * removing unused comments
    
    ---------
    
    Co-authored-by: nitish <nitish1814@github.com>

---
+++ common/core/src/test/java/zingg/common/core/executor/blockingverifier/data/BlockingVerifyData.java
@@ -0,0 +1,77 @@
+package zingg.common.core.executor.blockingverifier.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import zingg.common.core.executor.blockingverifier.model.BlockCountsData;
+import zingg.common.core.executor.blockingverifier.model.BlockedData;
+
+public class BlockingVerifyData {
+
+    public static List<BlockedData> getBlockedDF1() {
+
+        List<BlockedData> sample = new ArrayList<BlockedData>();
+        sample.add(new BlockedData("15", "rec-1040-dup-2", "matilda", "mestrov", "retiremen tvillage", "taringa", "qld", "2563135", "test", "-3910"));
+        sample.add(new BlockedData("101", "rec-1041-dup-0", "tyler", "frojd", "burramurra avenue", "san rmeo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("102", "rec-1041-org", "tyler", "froud", "burramurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("103", "rec-1041-dup-1", "tyler", "frjd", "burramura avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("98", "rec-1044-dup-3", "nicole", "carbone", "simpsonary barracks", "toowoomba", "wa", "8190756", "test", "3930"));
+        sample.add(new BlockedData("34", "rec-1042-dup-0", "kiandra", "", "rustenburg sth", "girgarre", "qld", "3328205", "test", "3925"));
+        sample.add(new BlockedData("123", "rec-1041-dup-21", "tyler", "frojd", "burramurra avenue", "san rem", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("124", "rec-1041-dup-22", "tyler", "froud", "burramurra avenue", "san remo", "sa", "781221", "test", "3915"));
+        sample.add(new BlockedData("125", "rec-1041-dup-23", "tyler", "frojd", "burramura avnue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("126", "rec-1041-dup-24", "tyler", "froud", "burramurra avenu", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("127", "rec-1041-dup-25", "tyler", "frojd", "burramurra aven", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("128", "rec-1041-dup-26", "tyler", "froud", "burramurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("129", "rec-1041-dup-27", "tyler", "frojd", "burramurra avenue", "san remo", "sa", "7812219", "", "3915"));
+        sample.add(new BlockedData("55", "rec-1043-org", "giorgia", "frahn", "ramano estate locn 1", "tallebudgera", "vic", "9724789", "test", "3950"));
+        sample.add(new BlockedData("19", "rec-1040-dup-1", "matilda", "mestrv", "retirement village", "taringa", "", "2563135", "test", "-3910"));
+        sample.add(new BlockedData("12", "rec-1040-dup-0", "matilda", "mestrov", "retirement village", "taringa", "qld", "2536135", "test", "-3910"));
+        sample.add(new BlockedData("94", "rec-1044-org", "nicole", "carbone", "simpson army barracks", "toowoomba", "wa", "8190756", "test", "3930"));
+        sample.add(new BlockedData("27", "rec-104-org", "benjamin", "carbone", "arthella", "orange", "vic", "3677127", "test", "-3945"));
+        sample.add(new BlockedData("92", "rec-1044-dup-1", "nicole", "carbone", "simpson army barracks", "toowoomba", "wa", "8190756", "test", "3930"));
+        sample.add(new BlockedData("96", "rec-1044-dup-0", "nicole", "shadbolt", "simpson army barracks", "toowoomba", "wa", "8190756", "test", "3930"));
+        sample.add(new BlockedData("11", "rec-1040-org", "matilda", "mestriv", "retirement village", "taringa", "qld", "2563135", "test", "-3910"));
+        sample.add(new BlockedData("39", "rec-1042-dup-1", "kiandra", "cowle", "rustenubr g sth", "girgarre", "qld", "3328205", "test", "3925"));
+        sample.add(new BlockedData("112", "rec-1041-dup-10", "tyler", "froud", "burramurra avenue", "san remo", "sa", "", "test", "3915"));
+        sample.add(new BlockedData("113", "rec-1041-dup-11", "tyler", "frojd", "burramura avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("114", "rec-1041-dup-12", "tyler", "froud", "burramura avene", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("115", "rec-1041-dup-13", "tyer", "frojd", "burramurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("116", "rec-1041-dup-14", "tyler", "froud", "buramurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("117", "rec-1041-dup-15", "tyler", "frojd", "burramura avenue", "san rmeo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("118", "rec-1041-dup-16", "tyler", "froud", "burramurra avenue", "san remo", "", "7812219", "test", "3915"));
+        sample.add(new BlockedData("119", "rec-1041-dup-17", "tylr", "frojd", "burramurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("120", "rec-1041-dup-18", "tyler", "froud", "burraurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("121", "rec-1041-dup-19", "tyler", "frojd", "burramurra avenue", "san remo", "sa", "781219", "test", "3915"));
+        sample.add(new BlockedData("122", "rec-1041-dup-20", "tyler", "frjd", "burramurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("23", "rec-104-dup-0", "benjaminl", "carbone", "arthella", "orange", "vic", "3677127", "test", "-3945"));
+        sample.add(new BlockedData("37", "rec-1042-org", "kiandra", "cowle", "rustenburg sth", "girgarre", "qld", "3328205", "test", "3925"));
+        sample.add(new BlockedData("95", "rec-1044-dup-2", "nicole", "carbone", "simpson arm ybarracks", "toowong", "wa", "8190756", "test", "3930"));
+        sample.add(new BlockedData("104", "rec-1041-dup-2", "tyler", "frod", "burramurra ave", "san rmeo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("105", "rec-1041-dup-3", "tyler", "frojd", "burramura avenu", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("106", "rec-1041-dup-4", "tyer", "froud", "burramura avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("107", "rec-1041-dup-5", "tylr", "frojd", "burramurra ave", "san rmeo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("108", "rec-1041-dup-6", "tyler", "foud", "buramurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("109", "rec-1041-dup-7", "tyler", "frojd", "burramurra avnue", "san rmeo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("110", "rec-1041-dup-8", "tyler", "frojd", "burrmurra avenue", "san remo", "sa", "7812219", "test", "3915"));
+        sample.add(new BlockedData("111", "rec-1041-dup-9", "tyler", "frojd", "burramurra avenue", "san rmo", "sa", "7812219", "test", "3915"));
+
+        return sample;
+    }
+
+    public static List<BlockCountsData> getExpectedBlockedDF1() {
+
+        List<BlockCountsData> sample = new ArrayList<BlockCountsData>();
+        sample.add(new BlockCountsData(3915, 29L)); 
+        sample.add(new BlockCountsData(3930, 5L));
+        sample.add(new BlockCountsData(-3910, 4L));
+        sample.add(new BlockCountsData(3925, 3L));
+        sample.add(new BlockCountsData(-3945, 2L));
+        sample.add(new BlockCountsData(3950, 1L));
+
+        return sample;
+    }
+
+
+    
+}
