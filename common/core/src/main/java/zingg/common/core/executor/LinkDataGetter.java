32eaed4c51b4990c81f09d5bbe4f34d23c7bd4df
Nitish <nitish@zingg.ai>
2025-03-17 22:59:17 +0530

    adjusted z_zid in source 2 input

--- common/core/src/main/java/zingg/common/core/executor/LinkDataGetter.java
+++ common/core/src/main/java/zingg/common/core/executor/LinkDataGetter.java
@@ -7,13 +7,14 @@ import zingg.common.client.model.LinkInputData;
 import zingg.common.client.util.PipeUtilBase;
 import zingg.common.core.match.data.IDataGetter;
 
-public class LinkDataGetter<S, D, R, C> implements IDataGetter<S, D, R, C> {
+public abstract class LinkDataGetter<S, D, R, C> implements IDataGetter<S, D, R, C> {
 
     @Override
     public LinkInputData<D, R, C> getData(IArguments arg, PipeUtilBase<S, D, R, C> p) throws ZinggClientException {
         ZFrame<D, R, C> sourceOneInput = p.read(true, true, arg.getNumPartitions(), true, arg.getData()[0]);
         ZFrame<D, R, C> sourceTwoInput = p.read(true, true, arg.getNumPartitions(), true, arg.getData()[1]);
-
-        return new LinkInputData<D, R, C>(sourceOneInput, sourceTwoInput);
+        return new LinkInputData<D, R, C>(sourceOneInput, getLineAdjustedDF(sourceTwoInput));
     }
+
+    protected abstract ZFrame<D, R, C> getLineAdjustedDF(ZFrame<D, R, C> df);
 }
