b3ed8cbb4737a2bc974601631d276d961f61f815
Nitish <nitish@zingg.ai>
2025-03-19 15:08:37 +0530

    return changed to GenericData instead of MatchInputData

--- common/core/src/main/java/zingg/common/core/match/data/DataGetter.java
+++ common/core/src/main/java/zingg/common/core/match/data/DataGetter.java
@@ -3,8 +3,8 @@ package zingg.common.core.match.data;
 import zingg.common.client.IArguments;
 import zingg.common.client.ZFrame;
 import zingg.common.client.ZinggClientException;
+import zingg.common.client.data.GenericData;
 import zingg.common.client.data.IData;
-import zingg.common.client.data.MatchInputData;
 import zingg.common.client.util.PipeUtilBase;
 
 public class DataGetter<S,D,R,C> implements IDataGetter<S,D,R,C>{
@@ -12,7 +12,7 @@ public class DataGetter<S,D,R,C> implements IDataGetter<S,D,R,C>{
     @Override
     public IData<D, R, C> getData(IArguments args, PipeUtilBase<S, D, R, C> p) throws ZinggClientException{
         ZFrame<D,R,C>  data = p.read(true, true, args.getNumPartitions(), true, args.getData());
-        return new MatchInputData<D, R, C>(data);
+        return new GenericData<D, R, C>(data);
     }
   
     
