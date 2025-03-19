f2607c13f570f98ab8554d4e19a9e64c8d08bccb
Nitish <nitish@zingg.ai>
2025-03-19 14:40:21 +0530

    made GenericData concrete

--- common/client/src/main/java/zingg/common/client/data/MatchInputData.java
+++ common/client/src/main/java/zingg/common/client/data/MatchInputData.java
@@ -2,7 +2,7 @@ package zingg.common.client.data;
 
 import zingg.common.client.ZFrame;
 
-public class MatchInputData<D, R, C> extends AGenericData<D, R, C> {
+public class MatchInputData<D, R, C> extends GenericData<D, R, C> {
 
     public MatchInputData(ZFrame<D, R, C> data) {
         super(data);
