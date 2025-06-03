438090ff73ca8d04ed12d34a7a2836d9d5f20a2c
Nitish <nitish@zingg.ai>
2025-06-03 16:30:40 +0530

    Added cache to Data (#1165)
    * removed commented code
    
    * added CONNECTED_COMPONENT_CHECKPOINT_INTERVAL env variable
    
    * renamed checkpoint interval variable
    
    * disable AQE by default
    
    * removed post processed link since handled in self pair source sensitive
    
    * removed method
    
    * report generated
    
    * report generated
    
    * removed checkpointing
    
    * removed variable
    
    * removed removed unused imports
    
    * refactored linker tests
    
    * removed changes for AQE from session builder
    
    * by default disable AQE
    
    * report generated
    
    * report generated
    
    * report generated
    
    * report generated
    
    * added cache to IData
    
    * added cache to IData
    
    ---------
    
    Co-authored-by: nitish <nitish1814@github.com>
    Co-authored-by: Sonal <sonalgoyal4@gmail.com>

--- common/core/src/main/java/zingg/common/core/data/DataImpl.java
+++ common/core/src/main/java/zingg/common/core/data/DataImpl.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class DataImpl<D, R, C> implements IData<D, R, C> {
 
     protected final List<ZFrame<D, R, C>> data;
+    private String name;
 
     public DataImpl(List<ZFrame<D, R, C>> data) {
         this.data = data;
@@ -28,6 +29,16 @@ public class DataImpl<D, R, C> implements IData<D, R, C> {
 
     public void addData(ZFrame<D, R, C> inputZFrame) {
         this.data.add(inputZFrame);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
@@ -51,6 +62,15 @@ public class DataImpl<D, R, C> implements IData<D, R, C> {
             count += zFrame.count();;
         }
         return count;
+    }
+
+    @Override
+    public IData<D, R, C> cache() {
+        List<ZFrame<D, R, C>> cachedZFrames = new ArrayList<>();
+        for (ZFrame<D, R, C> zFrame : data) {
+            cachedZFrames.add(zFrame.cache());
+        }
+        return new DataImpl<>(cachedZFrames);
     }
 
     @Override
