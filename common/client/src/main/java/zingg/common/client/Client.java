0c77d25d7d886934228b9279ecde53ac17ab7232
Nitish <nitish.joshi1995@gmail.com>
2025-08-18 04:32:37 +0530

    changes

--- common/client/src/main/java/zingg/common/client/Client.java
+++ common/client/src/main/java/zingg/common/client/Client.java
@@ -65,3 +65,63 @@ public abstract class Client<S,D,R,C> {
         return arguments;
     }
 }
+
+
+	public void addListener(Class<? extends IEvent> eventClass, IEventListener listener) {
+        EventsListener.getInstance().addListener(eventClass, listener);
+    }
+
+    public void initializeListeners() {
+        addListener(ZinggStartEvent.class, new ZinggStartListener());
+        addListener(ZinggStopEvent.class, new ZinggStopListener());
+    }
+    
+    public abstract S getSession();
+    
+    public void setSession(S s) {
+    	this.session = s;
+    }
+
+	public abstract PipeUtilBase<S, D, R, C> getPipeUtil();
+
+	public void setPipeUtil(PipeUtilBase<S, D, R, C> pipeUtil) {
+		this.pipeUtil = pipeUtil;
+	}
+
+	public String getProductName(){
+		return "Zingg AI";
+	}
+
+	public String getProductVersion(){
+		return "0.6.0";
+	}
+
+	public Long getMarkedRecordsStat(ZFrame<D,R,C>  markedRecords, long value) {
+		return zingg.getMarkedRecordsStat(markedRecords, value);
+	}
+
+	public Long getMatchedMarkedRecordsStat(ZFrame<D,R,C>  markedRecords) {
+		return zingg.getMatchedMarkedRecordsStat(markedRecords);
+	}
+
+	public Long getUnmatchedMarkedRecordsStat(ZFrame<D,R,C>  markedRecords) {
+		return zingg.getUnmatchedMarkedRecordsStat(markedRecords);
+	}
+
+	public Long getUnsureMarkedRecordsStat(ZFrame<D,R,C>  markedRecords) {
+		return zingg.getUnsureMarkedRecordsStat(markedRecords);
+	}
+
+	public ZFrame<D,R,C>  getMarkedRecords() {
+		return zingg.getMarkedRecords();
+	}
+
+	public ZFrame<D,R,C>  getUnmarkedRecords() {
+		return zingg.getUnmarkedRecords();
+	}
+
+	public ITrainingDataModel<S, D, R, C> getTrainingDataModel() throws UnsupportedOperationException {
+		return zingg.getTrainingDataModel();
+	}
+
+}
