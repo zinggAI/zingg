6255a7f9bd745674e2c9d5984fe182f895b444d8
Nitish <nitish.joshi1995@gmail.com>
2025-08-19 17:23:10 +0530

    removed inMemory read strategy

--- common/client/src/main/java/zingg/common/client/util/reader/ReadStrategyFactory.java
+++ common/client/src/main/java/zingg/common/client/util/reader/ReadStrategyFactory.java
@@ -2,15 +2,10 @@ package zingg.common.client.util.reader;
 
 import zingg.common.client.pipe.Pipe;
 import zingg.common.client.util.reader.impl.DefaultReadStrategy;
-import zingg.common.client.util.reader.impl.InMemoryReadStrategy;
 
 public class ReadStrategyFactory<D, R, C> {
     public ReadStrategy<D, R, C> getStrategy(Pipe<D, R, C> pipe) {
-        if (Pipe.FORMAT_INMEMORY.equals(pipe.getFormat())) {
-            return new InMemoryReadStrategy<>();
-        } else {
-            return new DefaultReadStrategy<>();
-        }
+        return new DefaultReadStrategy<>();
     }
 }
 
