663bdf0c90a2e9c3e7db644a233e3fee5a6ebfa7
Nitish <nitish.joshi1995@gmail.com>
2025-08-26 02:33:44 +0530

    added getters

--- common/client/src/main/java/zingg/common/client/Client.java
+++ common/client/src/main/java/zingg/common/client/Client.java
@@ -21,7 +21,7 @@ public abstract class Client<S,D,R,C> {
     public void init() throws ZinggClientException {
         banner.print(arguments.getCollectMetrics());
         zingg.init(arguments, sessionManager.get(), options);
-        initializeListeners();
+        ListenerManager.initializeListeners();
     }
 
     public void execute() throws ZinggClientException {
@@ -34,18 +34,6 @@ public abstract class Client<S,D,R,C> {
 
     public void stop() throws ZinggClientException {
         zingg.cleanup();
-    }
-
-    protected void initializeListeners() {
-        ListenerManager listenerManager = new ListenerManager();
-        listenerManager.initializeListeners();
-    }
-
-    public S getSession() {
-        return sessionManager.get();
-    }
-    public SessionManager<S> getSessionManager() {
-        return sessionManager;
     }
 
     public abstract PipeUtilBase<S, D, R, C> getPipeUtil();
@@ -64,64 +52,4 @@ public abstract class Client<S,D,R,C> {
     public IZArgs getArguments() {
         return arguments;
     }
-}
-
-
-	public void addListener(Class<? extends IEvent> eventClass, IEventListener listener) {
-        EventsListener.getInstance().addListener(eventClass, listener);
-    }
-
-    public void initializeListeners() {
-        addListener(ZinggStartEvent.class, new ZinggStartListener());
-        addListener(ZinggStopEvent.class, new ZinggStopListener());
-    }
-    
-    public abstract S getSession();
-    
-    public void setSession(S s) {
-    	this.session = s;
-    }
-
-	public abstract PipeUtilBase<S, D, R, C> getPipeUtil();
-
-	public void setPipeUtil(PipeUtilBase<S, D, R, C> pipeUtil) {
-		this.pipeUtil = pipeUtil;
-	}
-
-	public String getProductName(){
-		return "Zingg AI";
-	}
-
-	public String getProductVersion(){
-		return "0.6.0";
-	}
-
-	public Long getMarkedRecordsStat(ZFrame<D,R,C>  markedRecords, long value) {
-		return zingg.getMarkedRecordsStat(markedRecords, value);
-	}
-
-	public Long getMatchedMarkedRecordsStat(ZFrame<D,R,C>  markedRecords) {
-		return zingg.getMatchedMarkedRecordsStat(markedRecords);
-	}
-
-	public Long getUnmatchedMarkedRecordsStat(ZFrame<D,R,C>  markedRecords) {
-		return zingg.getUnmatchedMarkedRecordsStat(markedRecords);
-	}
-
-	public Long getUnsureMarkedRecordsStat(ZFrame<D,R,C>  markedRecords) {
-		return zingg.getUnsureMarkedRecordsStat(markedRecords);
-	}
-
-	public ZFrame<D,R,C>  getMarkedRecords() {
-		return zingg.getMarkedRecords();
-	}
-
-	public ZFrame<D,R,C>  getUnmarkedRecords() {
-		return zingg.getUnmarkedRecords();
-	}
-
-	public ITrainingDataModel<S, D, R, C> getTrainingDataModel() throws UnsupportedOperationException {
-		return zingg.getTrainingDataModel();
-	}
-
 }
