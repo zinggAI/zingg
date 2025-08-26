61ea072c374173659e3181e38170d87afaed4a6c
Nitish <nitish.joshi1995@gmail.com>
2025-08-26 12:39:37 +0530

    added back compatibility for location

--- common/client/src/main/java/zingg/common/client/util/reader/Helper.java
+++ common/client/src/main/java/zingg/common/client/util/reader/Helper.java
@@ -6,13 +6,21 @@ import java.util.Map;
 
 public class Helper {
 
+    private static final String PATH = "path";
+    private static final String LOCATION = "location";
+
     public static <D, R, C> IDFReader<D, R, C> initializeReaderForPipe(Pipe<D, R, C> pipe, IDFReader<D, R, C> dFReader) {
         IDFReader<D, R, C> reader = dFReader.format(pipe.getFormat());
         if (pipe.getSchema() != null) {
             reader = reader.setSchema(pipe.getSchema());
         }
         for (Map.Entry<String, String> entry : pipe.getProps().entrySet()) {
-            reader = reader.option(entry.getKey(), entry.getValue());
+            //back compatibility
+            if (LOCATION.equals(entry.getKey())) {
+                reader = reader.option(PATH, entry.getValue());
+            }   else {
+                reader = reader.option(entry.getKey(), entry.getValue());
+            }
         }
         return reader.option("mode", "PERMISSIVE");
     }
--- common/client/src/main/java/zingg/common/client/util/writer/Helper.java
+++ common/client/src/main/java/zingg/common/client/util/writer/Helper.java
@@ -4,12 +4,20 @@ import zingg.common.client.pipe.Pipe;
 
 public class Helper {
 
+    private static final String PATH = "path";
+    private static final String LOCATION = "location";
+
     public static <D, R, C> IDFWriter<D, R, C> initializeWriterForPipe(Pipe<D, R, C> pipe, IDFWriter<D, R, C> dfWriter) {
         IDFWriter<D, R, C> writer = dfWriter
                 .format(pipe.getFormat());
         writer.setMode(pipe.getMode() != null ? pipe.getMode() : "Append");
         for (String key : pipe.getProps().keySet()) {
-            writer = writer.option(key, pipe.get(key));
+            //back compatibility
+            if (LOCATION.equals(key)) {
+                writer = writer.option(PATH, pipe.get(key));
+            } else {
+                writer = writer.option(key, pipe.get(key));
+            }
         }
         return writer;
     }
