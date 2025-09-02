bf33525e8a84f1bb3ac66e023005c77a506c1d0f
Nitish <nitish@zingg.ai>
2025-09-02 17:05:17 +0530

    reformatted PipeUtil (#1183)
    * reformatted PipeUtil
    
    * break reader and  writer as two separate components
    
    * changes
    
    * changed variable scope
    
    * added Helper class for initializing reader
    
    * updated WriterStrategyFactory
    
    * updated PipeUtilBase
    
    * renamed interfaces names and added Helper to for initializing writer
    
    * removed loader with string
    
    * added default reader and writer strategy
    
    * removed inMemory read strategy
    
    * changed location to path property
    
    * removed in-memory pipes
    
    * made variable protected
    
    * updated strategy reader
    
    * added back compatibility for location
    
    * removed helper from reader and writer
    
    * added pipe to writer strategy
    
    * updated snowflake format
    
    * reverted back snowflake format
    
    * added constant location and path
    
    ---------
    
    Co-authored-by: Nitish <nitish.joshi1995@gmail.com>

--- common/client/src/main/java/zingg/common/client/pipe/FilePipe.java
+++ common/client/src/main/java/zingg/common/client/pipe/FilePipe.java
@@ -1,10 +1,9 @@
 package zingg.common.client.pipe;
 
 public class FilePipe {
-	
 	public static final String LOCATION = "location";
+	public static final String PATH = "path";
 	public static final String HEADER = "header";
 	public static final String DELIMITER = "delimiter";
 	public static final String TABLE = "table";
-
 }
