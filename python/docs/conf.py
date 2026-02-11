63771da07cd6f8b8f188c4c20cbe857cd4374faf
Sonal Goyal <sonalgoyal4@gmail.com>
2026-02-11 20:13:59 +0530

    trying relative links for docs

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -67,3 +67,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
+
+# -- Options for Markdown output ---------------------------------------------
+
+# Make links relative instead of absolute
+markdown_http_base = ''
+markdown_uri_doc_suffix = '.md'
