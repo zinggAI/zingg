5338d43dfe68610521138e69c5070104b3d57fe5
Aditya Pareek <119134401+LOGANBLUE1@users.noreply.github.com>
2026-02-18 17:12:08 +0530

    reverted changes

--- common/client/src/main/java/zingg/common/client/arguments/writer/JsonStringArgumentsWriter.java
+++ common/client/src/main/java/zingg/common/client/arguments/writer/JsonStringArgumentsWriter.java
@@ -3,16 +3,14 @@ package zingg.common.client.arguments.writer;
 import zingg.common.client.arguments.model.IZArgs;
 import zingg.common.client.ZinggClientException;
 
-import java.nio.file.Paths;
-
 public class JsonStringArgumentsWriter<A extends IZArgs> extends ArgumentsWriter<A> {
 
     @Override
-    public void write(String path, IZArgs args) throws ZinggClientException {
+    public void write(String sink, IZArgs args) throws ZinggClientException {
         try {
-            objectMapper.writeValue(Paths.get(path).toFile(), args);
+            sink = objectMapper.writeValueAsString(args);
         } catch (Exception exception) {
-            throw new ZinggClientException("Error writing config to jsonfile", exception);
+            throw new ZinggClientException("Error writing config to string", exception);
         }
     }
 }
