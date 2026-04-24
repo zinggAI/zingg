c762e372878eca0587d4fc10d01b5819024b27b6
Aditya Pareek <119134401+LOGANBLUE1@users.noreply.github.com>
2026-04-24 12:20:39 +0530

    -

--- spark/client/src/test/java/zingg/spark/client/TestArguments.java
+++ spark/client/src/test/java/zingg/spark/client/TestArguments.java
@@ -2,6 +2,7 @@ package zingg.spark.client;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.rmi.NoSuchObjectException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,48 +32,44 @@ public class TestArguments {
 		this.argumentService = new ArgumentServiceImpl<>(Arguments.class, new LoaderFactory<>(),new WriterFactory<>());
 	}
 	@Test
-	public void testWriteArgumentObjectToJSONFile() {
+	public void testWriteArgumentObjectToJSONFile() throws ZinggClientException, NoSuchObjectException {
 			IArguments args = new Arguments();
-			try {
-				FieldDefinition fname = new FieldDefinition();
-				fname.setFieldName("fname");
-				fname.setDataType("string");
-				fname.setMatchType(Arrays.asList(MatchTypes.EXACT, MatchTypes.FUZZY, MatchTypes.PINCODE));
-				//fname.setMatchType(Arrays.asList(MatchType.EXACT));
-				fname.setFields("fname");
-				FieldDefinition lname = new FieldDefinition();
-				lname.setFieldName("lname");
-				lname.setDataType("string");
-				lname.setMatchType(Arrays.asList(MatchTypes.FUZZY));
-				lname.setFields("lname");
-				args.setFieldDefinition(Arrays.asList(fname, lname));
+			FieldDefinition fname = new FieldDefinition();
+			fname.setFieldName("fname");
+			fname.setDataType("string");
+			fname.setMatchType(Arrays.asList(MatchTypes.EXACT, MatchTypes.FUZZY, MatchTypes.PINCODE));
+			//fname.setMatchType(Arrays.asList(MatchType.EXACT));
+			fname.setFields("fname");
+			FieldDefinition lname = new FieldDefinition();
+			lname.setFieldName("lname");
+			lname.setDataType("string");
+			lname.setMatchType(Arrays.asList(MatchTypes.FUZZY));
+			lname.setFields("lname");
+			args.setFieldDefinition(Arrays.asList(fname, lname));
 
-				Pipe inputPipe = new SparkPipe();
-				inputPipe.setName("test");
-				inputPipe.setFormat(Pipe.FORMAT_CSV);
-				inputPipe.setProp("path", "examples/febrl/test.csv");
-				args.setData(new Pipe[] {inputPipe});
+			Pipe inputPipe = new SparkPipe();
+			inputPipe.setName("test");
+			inputPipe.setFormat(Pipe.FORMAT_CSV);
+			inputPipe.setProp("path", "examples/febrl/test.csv");
+			args.setData(new Pipe[] {inputPipe});
 
-				Pipe outputPipe = new SparkPipe();
-				outputPipe.setName("output");
-				outputPipe.setFormat(Pipe.FORMAT_CSV);
-				outputPipe.setProp("path", "examples/febrl/output.csv");
-				args.setOutput(new Pipe[] {outputPipe});
+			Pipe outputPipe = new SparkPipe();
+			outputPipe.setName("output");
+			outputPipe.setFormat(Pipe.FORMAT_CSV);
+			outputPipe.setProp("path", "examples/febrl/output.csv");
+			args.setOutput(new Pipe[] {outputPipe});
 
-				args.setBlockSize(400L);
-				args.setCollectMetrics(true);
-				args.setModelId("500");
-				argumentService.loadArguments("/tmp/configFromArgObject.json");
+			args.setBlockSize(400L);
+			args.setCollectMetrics(true);
+			args.setModelId("500");
+			argumentService.loadArguments("/tmp/configFromArgObject.json");
 
-				//reload the same config file to check if deserialization is successful
-				IArguments newArgs = argumentService.loadArguments("/tmp/configFromArgObject.json");
-				assertEquals(newArgs.getModelId(), "500", "Model id is different");
-				assertEquals(newArgs.getBlockSize(), 400L, "Block size is different");
-				assertEquals(newArgs.getFieldDefinition().get(0).getFieldName(), "fname", "Field Definition[0]'s name is different");
-				List<IMatchType> expectedMatchType = Arrays.asList(MatchTypes.EXACT, MatchTypes.FUZZY, MatchTypes.PINCODE);
-				assertEquals(newArgs.getFieldDefinition().get(0).getMatchType(), expectedMatchType);
-			} catch (Exception | ZinggClientException e) {
-				e.printStackTrace();
-			}
+			//reload the same config file to check if deserialization is successful
+			IArguments newArgs = argumentService.loadArguments("/tmp/configFromArgObject.json");
+			assertEquals(newArgs.getModelId(), "500", "Model id is different");
+			assertEquals(newArgs.getBlockSize(), 400L, "Block size is different");
+			assertEquals(newArgs.getFieldDefinition().get(0).getFieldName(), "fname", "Field Definition[0]'s name is different");
+			List<IMatchType> expectedMatchType = Arrays.asList(MatchTypes.EXACT, MatchTypes.FUZZY, MatchTypes.PINCODE);
+			assertEquals(newArgs.getFieldDefinition().get(0).getMatchType(), expectedMatchType);
 		}
 }
